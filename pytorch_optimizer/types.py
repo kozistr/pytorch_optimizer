62985673a583f0d0daeab21150c7a241186b1dde
kozistr <kozistr@gmail.com>
2021-09-22 00:45:56 +0900

    update: PARAMS, STATE

--- pytorch_optimizer/types.py
+++ pytorch_optimizer/types.py
@@ -1,6 +1,10 @@
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import Any, Callable, Dict, Iterable, Optional, Tuple, Union
+
+import torch
 
 CLOSURE = Optional[Callable[[], float]]
 LOSS = Optional[float]
 BETAS = Tuple[float, float]
 DEFAULT_PARAMETERS = Dict[str, Any]
+PARAMS = Union[Iterable[torch.Tensor], Iterable[Dict[str, Any]]]
+STATE = Dict[str, Any]
