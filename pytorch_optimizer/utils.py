0c8a74e3cf1a4a5722ab9d36df24140b15f1f9d0
kozistr <kozistr@gmail.com>
2021-09-21 17:18:59 +0900

    feature: unit_norm

--- pytorch_optimizer/utils.py
+++ pytorch_optimizer/utils.py
@@ -1,10 +1,12 @@
+from typing import List, Optional, Tuple, Union
+
 import torch
 
 
 def normalize_gradient(
     x: torch.Tensor, use_channels: bool = False, epsilon: float = 1e-8
 ) -> torch.Tensor:
-    """ normalize gradient with stddev
+    """normalize gradient with stddev
     :param x: torch.Tensor. gradient.
     :param use_channels: bool. channel-wise normalization.
     :param epsilon: float. eps.
@@ -19,3 +21,21 @@ def normalize_gradient(
         s = x.std() + epsilon
         x.div_(s)
     return x
+
+
+def unit_norm(x: torch.Tensor) -> torch.Tensor:
+    keep_dim: bool = True
+    dim: Optional[Union[int, Tuple[int, ...]]] = None
+
+    x_len = len(x.shape)
+
+    if x_len <= 1:
+        keep_dim = False
+    elif x_len in (2, 3):  # linear layers
+        dim = 1
+    elif x_len == 4:  # conv kernels
+        dim = (1, 2, 3)
+    else:
+        dim = tuple([x for x in range(1, x_len)])
+
+    return x.norm(dim=dim, keepdim=keep_dim, p=2.0)
