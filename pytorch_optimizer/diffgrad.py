ce4bc288192eef768719df2b95b7f3d3cb2d1cf4
kozistr <kozistr@gmail.com>
2021-09-23 14:08:45 +0900

    refactor: DiffGrad

--- pytorch_optimizer/diffgrad.py
+++ pytorch_optimizer/diffgrad.py
@@ -3,6 +3,15 @@ import math
 import torch
 from torch.optim.optimizer import Optimizer
 
+from pytorch_optimizer.types import (
+    BETAS,
+    CLOSURE,
+    DEFAULT_PARAMETERS,
+    LOSS,
+    PARAMS,
+    STATE,
+)
+
 
 class DiffGrad(Optimizer):
     """
@@ -21,29 +30,42 @@ class DiffGrad(Optimizer):
     """
 
     def __init__(
-        self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0
+        self,
+        params: PARAMS,
+        lr: float = 1e-3,
+        betas: BETAS = (0.9, 0.999),
+        eps: float = 1e-8,
+        weight_decay: float = 0.0,
     ):
-        if not 0.0 <= lr:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
-        if not 0.0 <= betas[0] < 1.0:
-            raise ValueError(
-                "Invalid beta parameter at index 0: {}".format(betas[0])
-            )
-        if not 0.0 <= betas[1] < 1.0:
-            raise ValueError(
-                "Invalid beta parameter at index 1: {}".format(betas[1])
-            )
+        self.lr = lr
+        self.eps = eps
+        self.betas = betas
+        self.weight_decay = weight_decay
 
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        self.check_valid_parameters()
+
+        defaults: DEFAULT_PARAMETERS = dict(
+            lr=lr, betas=betas, eps=eps, weight_decay=weight_decay
+        )
         super().__init__(params, defaults)
 
-    def __setstate__(self, state):
+    def check_valid_parameters(self):
+        if 0.0 > self.lr:
+            raise ValueError(f'Invalid learning rate : {self.lr}')
+        if 0.0 > self.eps:
+            raise ValueError(f'Invalid eps : {self.eps}')
+        if 0.0 > self.weight_decay:
+            raise ValueError(f'Invalid weight_decay : {self.weight_decay}')
+        if not 0.0 <= self.betas[0] < 1.0:
+            raise ValueError(f'Invalid beta_0 : {self.betas[0]}')
+        if not 0.0 <= self.betas[1] < 1.0:
+            raise ValueError(f'Invalid beta_1 : {self.betas[1]}')
+
+    def __setstate__(self, state: STATE):
         super().__setstate__(state)
 
-    def step(self, closure=None):
-        loss = None
+    def step(self, closure: CLOSURE = None) -> LOSS:
+        loss: LOSS = None
         if closure is not None:
             loss = closure()
 
