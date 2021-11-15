cae55e9c952131d1f27b9fbb3f2ea38c0634cfa0
kozistr <kozistr@gmail.com>
2021-11-15 16:02:40 +0900

    feature: has_overflow & clip_grad_norm

--- pytorch_optimizer/utils.py
+++ pytorch_optimizer/utils.py
@@ -2,12 +2,18 @@ from typing import List, Optional, Tuple, Union
 
 import torch
 from torch import nn
+from torch.distributed import all_reduce
 
 from pytorch_optimizer.types import PARAMETERS
 
 
 def is_valid_parameters(parameters: PARAMETERS) -> bool:
     return isinstance(parameters, (list, tuple)) and len(parameters) > 0 and isinstance(parameters[0], dict)
+
+
+def has_overflow(grad_norm: torch.Tensor) -> bool:
+    """Detect inf and NaN in grad_norm."""
+    return grad_norm == float('inf') or grad_norm != grad_norm
 
 
 def normalize_gradient(x: torch.Tensor, use_channels: bool = False, epsilon: float = 1e-8) -> torch.Tensor:
@@ -25,6 +31,41 @@ def normalize_gradient(x: torch.Tensor, use_channels: bool = False, epsilon: flo
         s = x.std() + epsilon
         x.div_(s)
     return x
+
+
+def clip_grad_norm(parameters: PARAMETERS, max_norm: float = 0, sync: bool = False) -> torch.Tensor:
+    """Clips grad norms.
+    During combination with FSDP, will also ensure that grad norms are aggregated
+    across all workers, since each worker only stores their shard of the gradients.
+    :param parameters: Parameters whose gradients we wish to clip
+    :param max_norm: Maximum norm we wish the gradients to have. If non-positive, then
+        we will not perform clipping.
+    :param sync: Boolean indicating whether we should aggregate across the distributed
+        group. Used only in combination with FSDP.
+    :returns: The gradient norm across all parameters, before clipping.
+    """
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+
+    # make sure any generators are expanded
+    parameters = list(parameters)
+
+    # if syncing we need to manually perform the clipping so that we aggregate properly
+    if max_norm > 0 and not sync:
+        return torch.nn.utils.clip_grad_norm_(parameters, max_norm)
+
+    norm_sq = sum(p.grad.data.norm() ** 2 for p in parameters if p.grad is not None)
+    if sync:
+        # also need to get the norms from all the other sharded works in FSDP
+        all_reduce(norm_sq)
+
+    grad_norm = norm_sq.sqrt()
+    if max_norm > 0:
+        clip_coef = max_norm / (grad_norm + 1e-6)
+        for p in parameters:
+            p.grad.detach().mul_(clip_coef)
+
+    return grad_norm
 
 
 def unit_norm(x: torch.Tensor, norm: float = 2.0) -> torch.Tensor:
