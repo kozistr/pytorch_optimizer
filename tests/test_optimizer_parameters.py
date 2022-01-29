01dc14744a06f7dea6c1842d05956f219f40a5db
kozistr <kozistr@gmail.com>
2022-01-29 15:12:04 +0900

    update: test_betas

--- tests/test_optimizer_parameters.py
+++ tests/test_optimizer_parameters.py
@@ -4,7 +4,7 @@ import pytest
 
 from pytorch_optimizer import load_optimizers
 
-VALID_OPTIMIZER_NAMES: List[str] = [
+OPTIMIZER_NAMES: List[str] = [
     'adamp',
     'sgdp',
     'madgrad',
@@ -19,23 +19,47 @@ VALID_OPTIMIZER_NAMES: List[str] = [
     'lamb',
 ]
 
+BETA_OPTIMIZER_NAMES: List[str] = [
+    'adabelief',
+    'adabound',
+    'adahessian',
+    'admap',
+    'diffgrad',
+    'diffrgrad',
+    'lamb',
+    'radam',
+    'ranger',
+    'ranger21',
+]
 
-@pytest.mark.parametrize('optimizer_names', VALID_OPTIMIZER_NAMES)
+
+@pytest.mark.parametrize('optimizer_names', OPTIMIZER_NAMES)
 def test_learning_rate(optimizer_names):
     with pytest.raises(ValueError):
         optimizer = load_optimizers(optimizer_names)
         optimizer(None, lr=-1e-2)
 
 
-@pytest.mark.parametrize('optimizer_names', VALID_OPTIMIZER_NAMES)
+@pytest.mark.parametrize('optimizer_names', OPTIMIZER_NAMES)
 def test_epsilon(optimizer_names):
     with pytest.raises(ValueError):
         optimizer = load_optimizers(optimizer_names)
         optimizer(None, eps=-1e-6)
 
 
-@pytest.mark.parametrize('optimizer_names', VALID_OPTIMIZER_NAMES)
+@pytest.mark.parametrize('optimizer_names', OPTIMIZER_NAMES)
 def test_weight_decay(optimizer_names):
     with pytest.raises(ValueError):
         optimizer = load_optimizers(optimizer_names)
         optimizer(None, weight_decay=-1e-3)
+
+
+@pytest.mark.parametrize('optimizer_names', BETA_OPTIMIZER_NAMES)
+def test_betas(optimizer_names):
+    with pytest.raises(ValueError):
+        optimizer = load_optimizers(optimizer_names)
+        optimizer(None, betas=(-0.1, 0.1))
+
+    with pytest.raises(ValueError):
+        optimizer = load_optimizers(optimizer_names)
+        optimizer(None, betas=(0.1, -0.1))
