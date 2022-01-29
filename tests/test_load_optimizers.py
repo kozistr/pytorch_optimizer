11cb973defb58450bd108e90b1fe054c72432419
kozistr <kozistr@gmail.com>
2022-01-29 14:31:10 +0900

    update: test_weight_decay

--- tests/test_load_optimizers.py
+++ tests/test_load_optimizers.py
@@ -52,3 +52,10 @@ def test_epsilon(optimizer_names):
     with pytest.raises(ValueError):
         optimizer = load_optimizers(optimizer_names)
         optimizer(None, eps=-1e-6)
+
+
+@pytest.mark.parametrize('optimizer_names', VALID_OPTIMIZER_NAMES)
+def test_weight_decay(optimizer_names):
+    with pytest.raises(ValueError):
+        optimizer = load_optimizers(optimizer_names)
+        optimizer(None, weight_decay=-1e-3)
