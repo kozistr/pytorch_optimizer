0a84e5925c53d68a867fce23b11a2306846e2f93
kozistr <kozistr@gmail.com>
2022-02-01 13:55:47 +0900

    update: iterations to 500

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -95,7 +95,7 @@ OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (DiffRGrad, {'lr': 5e-1, 'weight_decay': 1e-3}, 200),
     (Lamb, {'lr': 1e-1, 'weight_decay': 1e-3}, 500),
     (Lamb, {'lr': 1e-1, 'weight_decay': 1e-3, 'pre_norm': True, 'eps': 1e-8}, 500),
-    (LARS, {'lr': 1e-1, 'weight_decay': 1e-3}, 200),
+    (LARS, {'lr': 1e-1, 'weight_decay': 1e-3}, 500),
     (RaLamb, {'lr': 1e-1, 'weight_decay': 1e-3}, 200),
     (MADGRAD, {'lr': 1e-2, 'weight_decay': 1e-3}, 500),
     (RAdam, {'lr': 1e-1, 'weight_decay': 1e-3}, 200),
