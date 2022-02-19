a134a71d36e9eadb260f4a9dccb3db679ddcb094
kozistr <kozistr@gmail.com>
2022-02-19 20:25:50 +0900

    update: base optimizer

--- pytorch_optimizer/diffrgrad.py
+++ pytorch_optimizer/diffrgrad.py
@@ -3,10 +3,11 @@ import math
 import torch
 from torch.optim.optimizer import Optimizer
 
+from pytorch_optimizer.base_optimizer import BaseOptimizer
 from pytorch_optimizer.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS, STATE
 
 
-class DiffRGrad(Optimizer):
+class DiffRGrad(Optimizer, BaseOptimizer):
     """
     Reference 1 : https://github.com/shivram1987/diffGrad
     Reference 2 : https://github.com/LiyuanLucasLiu/RAdam
@@ -52,7 +53,7 @@ class DiffRGrad(Optimizer):
         self.degenerated_to_sgd = degenerated_to_sgd
         self.eps = eps
 
-        self.check_valid_parameters()
+        self.validate_parameters()
 
         defaults: DEFAULTS = dict(
             lr=lr,
@@ -64,17 +65,11 @@ class DiffRGrad(Optimizer):
         )
         super().__init__(params, defaults)
 
-    def check_valid_parameters(self):
-        if self.lr < 0.0:
-            raise ValueError(f'Invalid learning rate : {self.lr}')
-        if self.weight_decay < 0.0:
-            raise ValueError(f'Invalid weight_decay : {self.weight_decay}')
-        if not 0.0 <= self.betas[0] < 1.0:
-            raise ValueError(f'Invalid beta_0 : {self.betas[0]}')
-        if not 0.0 <= self.betas[1] < 1.0:
-            raise ValueError(f'Invalid beta_1 : {self.betas[1]}')
-        if self.eps < 0.0:
-            raise ValueError(f'Invalid eps : {self.eps}')
+    def validate_parameters(self):
+        self.validate_learning_rate(self.lr)
+        self.validate_betas(self.betas)
+        self.validate_weight_decay(self.weight_decay)
+        self.validate_epsilon(self.eps)
 
     def __setstate__(self, state: STATE):
         super().__setstate__(state)
