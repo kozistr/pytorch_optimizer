3eedb46d6081a5ecd69d1df1c2956a8ba94f5b18
kozistr <kozistr@gmail.com>
2022-05-09 19:19:14 +0900

    update: test_get_supported_optimizers

--- tests/test_load_optimizers.py
+++ tests/test_load_optimizers.py
@@ -2,7 +2,7 @@ from typing import List
 
 import pytest
 
-from pytorch_optimizer import load_optimizer
+from pytorch_optimizer import get_supported_optimizers, load_optimizer
 
 VALID_OPTIMIZER_NAMES: List[str] = [
     'adamp',
@@ -43,3 +43,7 @@ def test_load_optimizers_valid(valid_optimizer_names):
 def test_load_optimizers_invalid(invalid_optimizer_names):
     with pytest.raises(NotImplementedError):
         load_optimizer(invalid_optimizer_names)
+
+
+def test_get_supported_optimizers():
+    assert len(get_supported_optimizers()) == 17
