b8da996b886bea2c719c99a5e7b8f44d2b9237db
kozistr <kozistr@gmail.com>
2022-05-09 19:03:23 +0900

    refactor: load_optimizer

--- pytorch_optimizer/__init__.py
+++ pytorch_optimizer/__init__.py
@@ -14,7 +14,6 @@ from pytorch_optimizer.lars import LARS
 from pytorch_optimizer.lookahead import Lookahead
 from pytorch_optimizer.madgrad import MADGRAD
 from pytorch_optimizer.nero import Nero
-from pytorch_optimizer.optimizers import load_optimizers
 from pytorch_optimizer.pcgrad import PCGrad
 from pytorch_optimizer.pnm import PNM
 from pytorch_optimizer.radam import RAdam
@@ -31,3 +30,44 @@ from pytorch_optimizer.utils import (
     normalize_gradient,
     unit_norm,
 )
+
+
+def load_optimizer(optimizer: str):  # pylint: disable=R0911
+    optimizer: str = optimizer.lower()
+
+    if optimizer == 'adamp':
+        return AdamP
+    if optimizer == 'ranger':
+        return Ranger
+    if optimizer == 'ranger21':
+        return Ranger21
+    if optimizer == 'sgdp':
+        return SGDP
+    if optimizer == 'radam':
+        return RAdam
+    if optimizer == 'adabelief':
+        return AdaBelief
+    if optimizer == 'adabound':
+        return AdaBound
+    if optimizer == 'madgrad':
+        return MADGRAD
+    if optimizer == 'diffgrad':
+        return DiffGrad
+    if optimizer == 'diffrgrad':
+        return DiffRGrad
+    if optimizer == 'lamb':
+        return Lamb
+    if optimizer == 'ralamb':
+        return RaLamb
+    if optimizer == 'lars':
+        return LARS
+    if optimizer == 'shampoo':
+        return Shampoo
+    if optimizer == 'pnm':
+        return PNM
+    if optimizer == 'adapnm':
+        return AdaPNM
+    if optimizer == 'nero':
+        return Nero
+
+    raise NotImplementedError(f'[-] not implemented optimizer : {optimizer}')
