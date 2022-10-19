4baafd931d057dad8af030a91c2e0b468891a362
kozistr <kozistr@gmail.com>
2022-10-19 17:58:15 +0900

    refactor: test cases

--- tests/test_lr_scheduler_parameters.py
+++ tests/test_lr_scheduler_parameters.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pytorch_optimizer import AdamP
+from pytorch_optimizer import AdamP, get_chebyshev_schedule
 from pytorch_optimizer.lr_scheduler.cosine_anealing import CosineAnnealingWarmupRestarts
 from tests.utils import Example
 
@@ -15,3 +15,8 @@ def test_cosine_annealing_warmup_restarts_params():
             first_cycle_steps=10,
             warmup_steps=20,
         )
+
+
+def test_chebyshev_params():
+    with pytest.raises(IndexError):
+        get_chebyshev_schedule(2)
