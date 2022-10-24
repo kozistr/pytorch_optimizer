f33781489f83e246e2d23e63b7fc5763c1482fa8
dowon <ks2515@naver.com>
2022-10-24 10:52:35 +0900

    update: hubconf support lr_scheduler

--- hubconf.py
+++ hubconf.py
@@ -7,14 +7,23 @@ Usage:
 from functools import partial as _partial
 from functools import update_wrapper as _update_wrapper
 
+from pytorch_optimizer import get_supported_lr_schedulers as _get_supported_lr_schedulers
 from pytorch_optimizer import get_supported_optimizers as _get_supported_optimizers
+from pytorch_optimizer import load_lr_scheduler as _load_lr_scheduler
 from pytorch_optimizer import load_optimizer as _load_optimizer
 
 dependencies = ['torch']
 
-for optimizer in _get_supported_optimizers():
-    name: str = optimizer.__name__
+for _optimizer in _get_supported_optimizers():
+    name: str = _optimizer.__name__
+    _func = _partial(_load_optimizer, optimizer=name)
+    _update_wrapper(_func, _optimizer)
     for n in (name, name.lower(), name.upper()):
-        func = _partial(_load_optimizer, optimizer=n)
-        _update_wrapper(func, optimizer)
-        globals()[n] = func
+        globals()[n] = _func
+
+for _scheduler in _get_supported_lr_schedulers():
+    name: str = _scheduler.__name__
+    _func = _partial(_load_lr_scheduler, lr_scheduler=name)
+    _update_wrapper(_func, _scheduler)
+    for n in (name, name.lower(), name.upper()):
+        globals()[n] = _func
