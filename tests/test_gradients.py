cc8569f9b599d6bc2a3dfd12585431700db6c9d2
kozistr <kozistr@gmail.com>
2023-01-03 06:32:31 +0900

    update: test_no_gradients

--- tests/test_gradients.py
+++ tests/test_gradients.py
@@ -17,6 +17,8 @@ def test_no_gradients(optimizer_name):
 
     if optimizer_name == 'ranger21':
         optimizer = load_optimizer(optimizer_name)(params, num_iterations=1)
+    elif optimizer_name == 'ralamb':
+        optimizer = load_optimizer(optimizer_name)(params, pre_norm=True)
     elif optimizer_name == 'lookahead':
         optimizer = Lookahead(load_optimizer('adamp')(params), k=1)
     else:
