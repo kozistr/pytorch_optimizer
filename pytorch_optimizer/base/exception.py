a242782570bcc0f5951cff392c26e8b437e18fa8
kozistr <kozistr@gmail.com>
2023-01-02 14:34:13 +0900

    update: NoSparseGradientError

--- pytorch_optimizer/base/exception.py
+++ pytorch_optimizer/base/exception.py
@@ -1,3 +1,8 @@
 class NoSparseGradientError(Exception):
-    """Raised when the gradient is sparse gradient"""
-    pass
+    """Raised when the gradient is sparse gradient
+
+    :param optimizer_name: str. name of the optimizer.
+    """
+    def __init__(self, optimizer_name: str):
+        self.message: str = f'[-] {optimizer_name} does not support sparse gradient.'
+        super().__init__(self.message)
