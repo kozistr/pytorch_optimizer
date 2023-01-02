e08cb4d23bc2a1799097ac228e6f04cef766a23c
kozistr <kozistr@gmail.com>
2023-01-02 15:46:24 +0900

    update: to main module

--- hubconf.py
+++ hubconf.py
@@ -11,13 +11,13 @@ dependencies = ['torch']
 for _optimizer in _get_supported_optimizers():
     name: str = _optimizer.__name__
     _func = _partial(_load_optimizer, optimizer=name)
-    _update_wrapper(_func, _optimizer.__init__)
+    _update_wrapper(_func, _optimizer)
     for n in (name, name.lower(), name.upper()):
         globals()[n] = _func
 
 for _scheduler in _get_supported_lr_schedulers():
     name: str = _scheduler.__name__
     _func = _partial(_load_lr_scheduler, lr_scheduler=name)
-    _update_wrapper(_func, _scheduler.__init__)
+    _update_wrapper(_func, _scheduler)
     for n in (name, name.lower(), name.upper()):
         globals()[n] = _func
