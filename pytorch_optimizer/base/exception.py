186a3b20521b9c67a6927e535bf48fe7fe9e7017
kozistr <kozistr@gmail.com>
2023-01-02 14:49:59 +0900

    update: ZeroParameterSize

--- pytorch_optimizer/base/exception.py
+++ pytorch_optimizer/base/exception.py
@@ -8,3 +8,11 @@ class NoSparseGradientError(Exception):
         self.note: str = ' ' if note == '' else f' w/ {note} '
         self.message: str = f'[-] {optimizer_name}{self.note}does not support sparse gradient.'
         super().__init__(self.message)
+
+
+class ZeroParameterSize(Exception):
+    """Raised when the parameter size is 0"""
+
+    def __init__(self):
+        self.message: str = '[-] parameter size is 0'
+        super().__init__(self.message)
