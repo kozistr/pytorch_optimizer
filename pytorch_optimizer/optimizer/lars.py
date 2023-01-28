fix: init with gradient, not parameter

pytorch_optimizer/optimizer/lars.py
@@ -102,7 +102,7 @@ class LARS(Optimizer, BaseOptimizer):
                 if group['momentum'] > 0.0:
                     param_state = self.state[p]
                     if 'mu' not in param_state:
-                        param_state['mu'] = torch.zeros_like(p, device=p.device)
+                        param_state['mu'] = grad.clone().detach()
 
                     mu = param_state['mu']
                     mu.mul_(group['momentum']).add_(grad, alpha=1.0 - group['dampening'])