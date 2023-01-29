9c9bedefcc672d8e01d059bef1e8533957961777
kozistr <kozistr@gmail.com>
2023-01-29 20:58:25 +0900

    style: fix ruff F541

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -180,7 +180,7 @@ def test_sam_optimizers_with_closure(adaptive, optimizer_sam_config):
 @pytest.mark.parametrize('adaptive', ADAPTIVE_FLAGS)
 def test_gsam_optimizers(adaptive):
     if not torch.cuda.is_available():
-        pytest.skip(f'there\'s no cuda. skip test.')
+        pytest.skip('there\'s no cuda. skip test.')
 
     (x_data, y_data), model, loss_fn = build_environment()
 
