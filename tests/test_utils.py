735b7385c8737aa1078fed1c517935806922e5f8
kozistr <kozistr@gmail.com>
2023-02-01 20:27:42 +0900

    update: test_compute_power

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -147,6 +147,13 @@ def test_compute_power():
         decimal=2,
     )
 
+    x = compute_power(torch.ones((2, 2)), p=16)
+    np.testing.assert_array_almost_equal(
+        np.asarray([[1.6142, -0.6567], [-0.6567, 1.6142]]),
+        x.numpy(),
+        decimal=2,
+    )
+
 
 def test_merge_small_dims():
     case1 = [1, 2, 512, 1, 2048, 1, 3, 4]
