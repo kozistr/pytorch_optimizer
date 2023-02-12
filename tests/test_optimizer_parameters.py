90f3e0f158caae4736354ff88454f62ac3222109
kozistr <kozistr@gmail.com>
2023-02-12 22:59:41 +0900

    update: test_adafactor_reset

--- tests/test_optimizer_parameters.py
+++ tests/test_optimizer_parameters.py
@@ -5,7 +5,7 @@ from torch import nn
 from pytorch_optimizer import SAM, Lookahead, PCGrad, Ranger21, SafeFP16Optimizer, load_optimizer
 from pytorch_optimizer.base.exception import NegativeLRError, NegativeStepError, ZeroParameterSizeError
 from tests.constants import BETA_OPTIMIZER_NAMES, PULLBACK_MOMENTUM, VALID_OPTIMIZER_NAMES
-from tests.utils import Example, simple_parameter
+from tests.utils import Example, LogisticRegression, simple_parameter
 
 
 @pytest.mark.parametrize('optimizer_name', VALID_OPTIMIZER_NAMES)
@@ -255,7 +255,10 @@ def test_ranger21_closure():
 
 
 def test_adafactor_reset():
-    optimizer = load_optimizer('adafactor')([simple_parameter(True)])
+    param = torch.zeros(1).requires_grad_(True)
+    param.grad = torch.zeros(1)
+
+    optimizer = load_optimizer('adafactor')([param])
     optimizer.reset()
 
 
