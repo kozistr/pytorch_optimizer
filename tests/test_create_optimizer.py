50295aa1294b5ca4c08caf6967f580207d8c1e3f
kozistr <kozistr@gmail.com>
2023-02-18 16:05:08 +0900

    update: test_create_optimizer

--- tests/test_create_optimizer.py
+++ tests/test_create_optimizer.py
@@ -5,5 +5,5 @@ from tests.utils import LogisticRegression
 def test_create_optimizer():
     model = LogisticRegression()
 
-    optimizer = create_optimizer(model, 'adamp', lr=1e-2, weight_decay=1e-3, use_gc=True, use_lookahead=True)
-    optimizer = create_optimizer(model, 'alig', lr=1e-2, use_lookahead=True)
+    create_optimizer(model, 'adamp', lr=1e-2, weight_decay=1e-3, use_gc=True, use_lookahead=True)
+    create_optimizer(model, 'alig', lr=1e-2, use_lookahead=True)
