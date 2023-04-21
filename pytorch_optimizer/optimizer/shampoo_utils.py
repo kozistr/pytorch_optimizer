c88270a329f28139813a2e370bd1a03988d379c0
kozistr <kozistr@gmail.com>
2023-04-21 17:40:42 +0900

    update: compute_power_schur_newton

--- pytorch_optimizer/optimizer/shampoo_utils.py
+++ pytorch_optimizer/optimizer/shampoo_utils.py
@@ -468,7 +468,6 @@ def compute_power_schur_newton(
     error = torch.max(torch.abs(mat_m - identity))
 
     new_mat_root = torch.empty_like(mat_root)
-    new_error = torch.empty_like(mat_m)
 
     for _ in range(max_iters):
         mat_m_i = alpha_identity + alpha * mat_m
@@ -476,7 +475,7 @@ def compute_power_schur_newton(
         torch.matmul(mat_root, mat_m_i, out=new_mat_root)
         torch.matmul(torch.linalg.matrix_power(mat_m_i, p), mat_m, out=mat_m)
 
-        torch.max(torch.abs(mat_m - identity), out=new_error)
+        new_error = torch.max(torch.abs(mat_m - identity))
         if new_error <= error_tolerance or new_error > error * max_error_ratio:
             break
 
