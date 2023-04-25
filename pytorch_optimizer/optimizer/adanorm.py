20d1c63a793b84b72545861f36b8e39f865b81e8
kozistr <kozistr@gmail.com>
2023-04-25 15:06:56 +0900

    refactor: features into group

--- pytorch_optimizer/optimizer/adanorm.py
+++ pytorch_optimizer/optimizer/adanorm.py
@@ -18,8 +18,8 @@ class AdaNorm(Optimizer, BaseOptimizer):
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
     :param fixed_decay: bool. fix weight decay.
-    :param adamd_debias_term: bool. Only correct the denominator to avoid inflating step sizes early in training.
     :param amsgrad: bool. whether to use the AMSGrad variant of this algorithm from the paper.
+    :param adamd_debias: bool. Only correct the denominator to avoid inflating step sizes early in training.
     :param eps: float. term added to the denominator to improve numerical stability.
     """
 
@@ -32,23 +32,28 @@ class AdaNorm(Optimizer, BaseOptimizer):
         weight_decay: float = 0.0,
         weight_decouple: bool = True,
         fixed_decay: bool = False,
-        adamd_debias_term: bool = False,
         amsgrad: bool = False,
+        adamd_debias: bool = False,
         eps: float = 1e-8,
     ):
         self.lr = lr
         self.betas = betas
-        self.r = r
         self.weight_decay = weight_decay
-        self.weight_decouple = weight_decouple
-        self.fixed_decay = fixed_decay
-        self.adamd_debias_term = adamd_debias_term
-        self.amsgrad = amsgrad
         self.eps = eps
 
         self.validate_parameters()
 
-        defaults: DEFAULTS = {'lr': lr, 'betas': betas, 'weight_decay': weight_decay}
+        defaults: DEFAULTS = {
+            'lr': lr,
+            'betas': betas,
+            'r': r,
+            'weight_decay': weight_decay,
+            'weight_decouple': weight_decouple,
+            'fixed_decay': fixed_decay,
+            'amsgrad': amsgrad,
+            'adamd_debias': adamd_debias,
+            'eps': eps,
+        }
         super().__init__(params, defaults)
 
     def validate_parameters(self):
@@ -70,7 +75,7 @@ class AdaNorm(Optimizer, BaseOptimizer):
                 state['exp_avg'] = torch.zeros_like(p)
                 state['exp_avg_var'] = torch.zeros_like(p)
                 state['exp_grad_norm'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
-                if self.amsgrad:
+                if group['amsgrad']:
                     state['max_exp_avg_var'] = torch.zeros_like(p)
 
     @torch.no_grad()
@@ -105,19 +110,18 @@ class AdaNorm(Optimizer, BaseOptimizer):
                     state['exp_avg'] = torch.zeros_like(p)
                     state['exp_avg_var'] = torch.zeros_like(p)
                     state['exp_grad_norm'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
-                    if self.amsgrad:
+                    if group['amsgrad']:
                         state['max_exp_avg_var'] = torch.zeros_like(p)
 
-                if self.weight_decouple:
-                    p.mul_(1.0 - group['weight_decay'] * (1 if self.fixed_decay else group['lr']))
+                if group['weight_decouple']:
+                    p.mul_(1.0 - group['weight_decay'] * (1.0 if group['fixed_decay'] else group['lr']))
                 elif group['weight_decay'] > 0.0:
                     grad.add_(p, alpha=group['weight_decay'])
 
-                # gradient norm correction (GNC)
                 grad_norm = torch.linalg.norm(grad)
 
                 exp_grad_norm = state['exp_grad_norm']
-                exp_grad_norm.mul_(self.r).add_(grad_norm, alpha=1.0 - self.r)
+                exp_grad_norm.mul_(group['r']).add_(grad_norm, alpha=1.0 - group['r'])
 
                 s_grad = grad
                 if exp_grad_norm > grad_norm:
@@ -127,7 +131,7 @@ class AdaNorm(Optimizer, BaseOptimizer):
                 exp_avg.mul_(beta1).add_(s_grad, alpha=1.0 - beta1)
                 exp_avg_var.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
 
-                if self.amsgrad:
+                if group['amsgrad']:
                     max_exp_avg_var = state['max_exp_avg_var']
                     torch.max(max_exp_avg_var, exp_avg_var, out=max_exp_avg_var)
                     de_nom = max_exp_avg_var.add(self.eps).sqrt()
@@ -136,7 +140,7 @@ class AdaNorm(Optimizer, BaseOptimizer):
 
                 de_nom.div_(bias_correction2_sq).add_(self.eps)
 
-                step_size = group['lr'] if self.adamd_debias_term else group['lr'] / bias_correction1
+                step_size = group['lr'] if group['adamd_debias'] else group['lr'] / bias_correction1
                 p.addcdiv_(exp_avg, de_nom, value=-step_size)
 
         return loss
