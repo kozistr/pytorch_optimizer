435b563a13aa0fd932230c1b8a23dff3479df112
kozistr <kozistr@gmail.com>
2023-04-25 15:58:52 +0900

    update: step

--- pytorch_optimizer/optimizer/adapnm.py
+++ pytorch_optimizer/optimizer/adapnm.py
@@ -81,8 +81,17 @@ class AdaPNM(Optimizer, BaseOptimizer):
                 loss = closure()
 
         for group in self.param_groups:
+            if 'step' in group:
+                group['step'] += 1
+            else:
+                group['step'] = 1
+
             beta1, beta2, beta3 = group['betas']
             noise_norm = math.sqrt((1 + beta3) ** 2 + beta3 ** 2)  # fmt: skip
+
+            bias_correction1 = 1 - beta1 ** group['step']
+            bias_correction2_sq = math.sqrt(1 - beta2 ** group['step'])
+
             for p in group['params']:
                 if p.grad is None:
                     continue
@@ -98,24 +107,18 @@ class AdaPNM(Optimizer, BaseOptimizer):
 
                 state = self.state[p]
                 if len(state) == 0:
-                    state['step'] = 0
                     state['exp_avg'] = torch.zeros_like(p)
                     state['exp_avg_sq'] = torch.zeros_like(p)
                     state['neg_exp_avg'] = torch.zeros_like(p)
                     if group['amsgrad']:
                         state['max_exp_avg_sq'] = torch.zeros_like(p)
 
-                state['step'] += 1
-
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2_sq = math.sqrt(1 - beta2 ** state['step'])
-
-                exp_avg_sq = state['exp_avg_sq']
                 if state['step'] % 2 == 1:
                     exp_avg, neg_exp_avg = state['exp_avg'], state['neg_exp_avg']
                 else:
                     exp_avg, neg_exp_avg = state['neg_exp_avg'], state['exp_avg']
 
+                exp_avg_sq = state['exp_avg_sq']
                 exp_avg.mul_(beta1 ** 2).add_(grad, alpha=1.0 - beta1 ** 2)  # fmt: skip
                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
 
