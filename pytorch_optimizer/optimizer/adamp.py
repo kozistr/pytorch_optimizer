f20a1585b42cb78c27cb1e412e637869aae58a9a
kozistr <kozistr@gmail.com>
2023-04-25 15:22:49 +0900

    fix: bias_correction2_sq

--- pytorch_optimizer/optimizer/adamp.py
+++ pytorch_optimizer/optimizer/adamp.py
@@ -95,7 +95,7 @@ class AdamP(Optimizer, BaseOptimizer):
 
             beta1, beta2 = group['betas']
             bias_correction1 = 1.0 - beta1 ** group['step']
-            bias_correction2_sq = math.sqrt(1.0 - group ** state['step'])
+            bias_correction2_sq = math.sqrt(1.0 - beta2 ** group['step'])
 
             for p in group['params']:
                 if p.grad is None:
