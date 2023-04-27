b3e36a226d9072bf536ef340849d33f7fe33a418
kozistr <kozistr@gmail.com>
2023-04-27 20:49:39 +0900

    feature: AdaNorm variant

--- pytorch_optimizer/optimizer/lion.py
+++ pytorch_optimizer/optimizer/lion.py
@@ -4,6 +4,7 @@ from torch.optim.optimizer import Optimizer
 from pytorch_optimizer.base.exception import NoSparseGradientError
 from pytorch_optimizer.base.optimizer import BaseOptimizer
 from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS
+from pytorch_optimizer.optimizer.gc import centralize_gradient
 
 
 class Lion(Optimizer, BaseOptimizer):
@@ -14,6 +15,9 @@ class Lion(Optimizer, BaseOptimizer):
     :param betas: BETAS. coefficients used for computing running averages of gradient and the squared hessian trace.
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
+    :param use_gc: bool. use gradient centralization.
+    :param r: float. EMA factor. between 0.9 ~ 0.99 is preferred.
+    :param adanorm: bool. whether to use the AdaNorm variant.
     """
 
     def __init__(
@@ -23,10 +27,14 @@ class Lion(Optimizer, BaseOptimizer):
         betas: BETAS = (0.9, 0.99),
         weight_decay: float = 0.0,
         weight_decouple: bool = True,
+        use_gc: bool = False,
+        r: float = 0.95,
+        adanorm: bool = False,
     ):
         self.lr = lr
         self.betas = betas
         self.weight_decay = weight_decay
+        self.use_gc = use_gc
 
         self.validate_parameters()
 
@@ -35,7 +43,11 @@ class Lion(Optimizer, BaseOptimizer):
             'betas': betas,
             'weight_decay': weight_decay,
             'weight_decouple': weight_decouple,
+            'adanorm': adanorm,
         }
+        if adanorm:
+            defaults.update({'r': r})
+
         super().__init__(params, defaults)
 
     def validate_parameters(self):
@@ -53,6 +65,8 @@ class Lion(Optimizer, BaseOptimizer):
                 state = self.state[p]
 
                 state['exp_avg'] = torch.zeros_like(p)
+                if group['adanorm']:
+                    state['exp_grad_norm'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
 
     @torch.no_grad()
     def step(self, closure: CLOSURE = None) -> LOSS:
@@ -76,6 +90,11 @@ class Lion(Optimizer, BaseOptimizer):
 
                 if len(state) == 0:
                     state['exp_avg'] = torch.zeros_like(p)
+                    if group['adanorm']:
+                        state['exp_grad_norm'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
+
+                if self.use_gc:
+                    grad = centralize_gradient(grad, gc_conv_only=False)
 
                 exp_avg = state['exp_avg']
 
@@ -85,9 +104,19 @@ class Lion(Optimizer, BaseOptimizer):
                     else:
                         grad.add_(p, alpha=weight_decay)
 
+                s_grad = grad
+                if group['adanorm']:
+                    grad_norm = torch.linalg.norm(grad)
+
+                    exp_grad_norm = state['exp_grad_norm']
+                    exp_grad_norm.mul_(group['r']).add_(grad_norm, alpha=1.0 - group['r'])
+
+                    if exp_grad_norm > grad_norm:
+                        s_grad *= exp_grad_norm / grad_norm
+
                 update = exp_avg.clone()
                 update.mul_(beta1).add_(grad, alpha=1.0 - beta1).sign_()
-                exp_avg.mul_(beta2).add_(grad, alpha=1.0 - beta2)
+                exp_avg.mul_(beta2).add_(s_grad, alpha=1.0 - beta2)
 
                 p.add_(update, alpha=-group['lr'])
 
