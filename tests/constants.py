aafe341ba761c4bea7248bd4834fe2cd98b53375
kozistr <kozistr@gmail.com>
2023-04-28 16:24:49 +0900

    update: recipes

--- tests/constants.py
+++ tests/constants.py
@@ -6,6 +6,7 @@ from pytorch_optimizer import (
     OPTIMIZERS,
     PNM,
     SGDP,
+    SGDW,
     SM3,
     A2Grad,
     AccSGD,
@@ -34,7 +35,6 @@ from pytorch_optimizer import (
     Ranger21,
     ScalableShampoo,
     Shampoo,
-SGDW,
 )
 from tests.utils import build_lookahead
 
@@ -318,7 +318,7 @@ OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (A2Grad, {'variant': 'exp', 'beta': 1.0, 'lips': 1.0, 'rho': 0.9}, 5),
     (AccSGD, {'lr': 1e-0, 'weight_decay': 1e-3}, 5),
     (SGDW, {'lr': 5e-1, 'momentum': 0.9, 'weight_decay': 1e-3}, 5),
-    (SGDW, {'lr': 5e-1, 'momentum': 0.9, 'weight_decay': 1e-3, 'weight_decoupled': False}, 5),
+    (SGDW, {'lr': 5e-1, 'momentum': 0.9, 'weight_decay': 1e-3, 'weight_decouple': False}, 5),
 ]
 ADANORM_SUPPORTED_OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (AdaBelief, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 10),
