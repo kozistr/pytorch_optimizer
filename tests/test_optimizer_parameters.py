a473c03182453f76b6bcecfd336ddebefd79134b
kozistr <kozistr@gmail.com>
2023-04-28 14:20:26 +0900

    update: test_weight_decay

--- tests/test_optimizer_parameters.py
+++ tests/test_optimizer_parameters.py
@@ -66,7 +66,7 @@ def test_adafactor_epsilon():
 
 @pytest.mark.parametrize('optimizer_name', VALID_OPTIMIZER_NAMES)
 def test_weight_decay(optimizer_name):
-    if optimizer_name in ('nero', 'alig', 'sm3'):
+    if optimizer_name in ('nero', 'alig', 'sm3', 'a2grad'):
         pytest.skip(f'skip {optimizer_name} optimizer')
 
     optimizer = load_optimizer(optimizer_name)
