771ceec80f1a4c7c819c34b3723525cf57d91ef3
kozistr <kozistr@gmail.com>
2023-04-28 17:08:51 +0900

    docs: ASGD docstring

--- pytorch_optimizer/optimizer/sgd.py
+++ pytorch_optimizer/optimizer/sgd.py
@@ -213,6 +213,7 @@ class ASGD(Optimizer, BaseOptimizer):
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
     :param dampening: float. dampening for momentum.
+    :param eps: float. term added to the denominator to improve numerical stability.
     """
 
     def __init__(
