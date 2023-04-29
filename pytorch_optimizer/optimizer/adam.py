1ec4aa887397dfe306784e30519f9bd24a8b1c63
kozistr <kozistr@gmail.com>
2023-04-29 16:44:13 +0900

    feature: QHAdam optimizer

---
+++ pytorch_optimizer/optimizer/adam.py
@@ -0,0 +1,138 @@
+import math
+from typing import Tuple
+
+import torch
+from torch.optim.optimizer import Optimizer
+
+from pytorch_optimizer.base.exception import NoSparseGradientError
+from pytorch_optimizer.base.optimizer import BaseOptimizer
+from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS
+
+
+class QHAdam(Optimizer, BaseOptimizer):
+    r"""Quasi-hyperbolic momentum and Adam for deep learning.
+
+    :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
+    :param lr: float. learning rate.
+    :param betas: BETAS. coefficients used for computing running averages of gradient and the squared hessian trace.
+    :param nus: Tuple[float, float]. immediate discount factors used to estimate the gradient and its square.
+    :param weight_decay: float. weight decay (L2 penalty).
+    :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
+    :param adam_debias: bool. Only correct the denominator to avoid inflating step sizes early in training.
+    :param eps: float. term added to the denominator to improve numerical stability.
+    """
+
+    def __init__(
+        self,
+        params: PARAMETERS,
+        lr: float = 1e-3,
+        betas: BETAS = (0.9, 0.999),
+        nus: Tuple[float, float] = (1.0, 1.0),
+        weight_decay: float = 0.0,
+        weight_decouple: bool = False,
+        adam_debias: bool = False,
+        eps: float = 1e-8,
+    ):
+        self.lr = lr
+        self.betas = betas
+        self.nus = nus
+        self.weight_decay = weight_decay
+        self.eps = eps
+
+        self.validate_parameters()
+
+        defaults: DEFAULTS = {
+            'lr': lr,
+            'betas': betas,
+            'nus': nus,
+            'weight_decay': weight_decay,
+            'weight_decouple': weight_decouple,
+            'adam_debias': adam_debias,
+            'eps': eps,
+        }
+        super().__init__(params, defaults)
+
+    def validate_parameters(self):
+        self.validate_learning_rate(self.lr)
+        self.validate_betas(self.betas)
+        self.validate_weight_decay(self.weight_decay)
+        self.validate_epsilon(self.eps)
+
+    def __str__(self) -> str:
+        return 'QHAdam'
+
+    @torch.no_grad()
+    def reset(self):
+        for group in self.param_groups:
+            group['step'] = 0
+            for p in group['params']:
+                state = self.state[p]
+
+                state['beta1_weight'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
+                state['beta2_weight'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
+                state['exp_avg'] = torch.zeros_like(p)
+                state['exp_avg_sq'] = torch.zeros_like(p)
+
+    @torch.no_grad()
+    def step(self, closure: CLOSURE = None) -> LOSS:
+        loss: LOSS = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            if 'step' in group:
+                group['step'] += 1
+            else:
+                group['step'] = 1
+
+            beta1, beta2 = group['betas']
+            nu1, nu2 = group['nus']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                grad = p.grad
+                if grad.is_sparse:
+                    raise NoSparseGradientError(str(self))
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['beta1_weight'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
+                    state['beta2_weight'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
+                    state['exp_avg'] = torch.zeros_like(p)
+                    state['exp_avg_sq'] = torch.zeros_like(p)
+
+                if group['weight_decouple']:
+                    p.mul_(1.0 - group['weight_decay'] * group['lr'])
+                elif group['weight_decay'] > 0.0:
+                    grad.add_(p, alpha=group['weight_decay'])
+
+                beta1_weight, beta2_weight = state['beta1_weight'], state['beta2_weight']
+                beta1_weight.mul_(beta1).add_(1.0)
+                beta2_weight.mul_(beta2).add_(1.0)
+
+                beta1_adj = 1.0 - (1.0 / beta1_weight)
+                beta2_adj = 1.0 - (1.0 / beta2_weight)
+
+                grad_p2 = grad.pow(2)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg.mul_(beta1_adj).add_(grad, alpha=1.0 - beta1_adj)
+                exp_avg_sq.mul_(beta2_adj).add_(grad_p2, value=1.0 - beta2_adj)
+
+                avg_grad = exp_avg.mul(nu1)
+                if nu1 != 1.0:
+                    avg_grad.add_(grad, alpha=1.0 - nu1)
+
+                avg_grad_rms = exp_avg_sq.mul(nu2)
+                if nu2 != 1.0:
+                    avg_grad_rms.add_(grad_p2, alpha=1.0 - nu2)
+
+                avg_grad_rms.sqrt_().add_(group['eps'])
+
+                p.addcdiv_(avg_grad, avg_grad_rms, value=-group['lr'])
+
+        return loss
