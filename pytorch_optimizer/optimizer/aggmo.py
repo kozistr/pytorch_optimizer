f12318b27352659295a7ace8b325d4625fe91d1f
kozistr <kozistr@gmail.com>
2023-04-29 16:01:23 +0900

    update: weight_decouple to False

--- pytorch_optimizer/optimizer/aggmo.py
+++ pytorch_optimizer/optimizer/aggmo.py
@@ -22,7 +22,7 @@ class AggMo(Optimizer, BaseOptimizer):
         lr: float = 1e-3,
         betas: BETAS = (0.0, 0.9, 0.99),
         weight_decay: float = 0.0,
-        weight_decouple: bool = True,
+        weight_decouple: bool = False,
     ):
         self.lr = lr
         self.betas = betas
