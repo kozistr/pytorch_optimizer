fc1705575d24240677f7c97961bfd6767fee0077
kozistr <kozistr@gmail.com>
2023-04-29 15:43:56 +0900

    update: test_get_supported_optimizers

--- tests/test_load_optimizers.py
+++ tests/test_load_optimizers.py
@@ -16,4 +16,4 @@ def test_load_optimizers_invalid(invalid_optimizer_names):
 
 
 def test_get_supported_optimizers():
-    assert len(get_supported_optimizers()) == 39
+    assert len(get_supported_optimizers()) == 40
