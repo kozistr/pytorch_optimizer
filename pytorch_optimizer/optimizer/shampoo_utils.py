696183ea8d58b2f08daf0399f1af18228c6bb108
kozistr <kozistr@gmail.com>
2023-04-30 21:57:43 +0900

    update: merge_partitions

--- pytorch_optimizer/optimizer/shampoo_utils.py
+++ pytorch_optimizer/optimizer/shampoo_utils.py
@@ -187,7 +187,7 @@ class BlockPartitioner:
             n: int = len(indices) + 1
 
             partitions: List[torch.Tensor] = [
-                torch.cat(partitions[idx:idx + n], axis=i) for idx in range(0, len(partitions), n)  # fmt: skip
+                torch.cat(partitions[idx:idx + n], dim=i) for idx in range(0, len(partitions), n)  # fmt: skip
             ]
 
         # TODO
