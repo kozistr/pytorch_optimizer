67d47d0400135176bbdf887ad8bd4a5ad2b606b6
kozistr <kozistr@gmail.com>
2023-05-02 15:19:05 +0900

    refactor: reset()

--- pytorch_optimizer/optimizer/a2grad.py
+++ pytorch_optimizer/optimizer/a2grad.py
@@ -29,24 +29,17 @@ class A2Grad(Optimizer, BaseOptimizer):
         rho: float = 0.5,
         variant: str = 'uni',
     ):
-        self.lr = lr
-        self.beta = beta
-        self.lips = lips
-        self.rho = rho
-        self.variant = variant
+        self.validate_learning_rate(lr)
+        self.validate_lipschitz_constant(lips)
+        self.validate_rho(rho)
+        self.validate_a2grad_variant(variant)
 
-        self.validate_parameters()
-
-        defaults: DEFAULTS = {'beta': beta, 'lips': lips}
+        defaults: DEFAULTS = {'beta': beta, 'lips': lips, 'variant': variant}
         if variant == 'exp':
             defaults.update({'rho': rho})
 
         super().__init__(params, defaults)
-
-    def validate_parameters(self):
-        self.validate_lipschitz_constant(self.lips)
-        self.validate_rho(self.rho)
-        self.validate_a2grad_variant(self.variant)
+        self.reset()
 
     def __str__(self) -> str:
         return 'A2Grad'
@@ -59,7 +52,7 @@ class A2Grad(Optimizer, BaseOptimizer):
                 state = self.state[p]
 
                 state['alpha_k'] = 1.0
-                state['v_k'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
+                state['v_k'] = torch.zeros((1,), dtype=p.grad.dtype, device=p.grad.device)
                 state['avg_grad'] = torch.zeros_like(p)
                 state['x_k'] = p.clone()
 
@@ -71,10 +64,7 @@ class A2Grad(Optimizer, BaseOptimizer):
                 loss = closure()
 
         for group in self.param_groups:
-            if 'step' in group:
-                group['step'] += 1
-            else:
-                group['step'] = 1
+            group['step'] += 1
 
             gamma_k: float = 2.0 * group['lips'] / (group['step'] + 1)
             alpha_k_1: float = 2.0 / (group['step'] + 3)
@@ -89,14 +79,6 @@ class A2Grad(Optimizer, BaseOptimizer):
 
                 state = self.state[p]
 
-                if len(state) == 0:
-                    state['alpha_k'] = 1.0
-                    state['v_k'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
-                    state['avg_grad'] = grad.clone()
-                    state['x_k'] = p.clone()
-                    if self.variant == 'exp':
-                        state['v_kk'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
-
                 avg_grad = state['avg_grad']
                 avg_grad.add_(grad - avg_grad, alpha=group['step'] + 1)
 
@@ -106,8 +88,8 @@ class A2Grad(Optimizer, BaseOptimizer):
                 delta_k_sq = delta_k.pow(2).sum()
 
                 v_k = state['v_k']
-                if self.variant in ('uni', 'inc'):
-                    if self.variant == 'inc':
+                if group['variant'] in ('uni', 'inc'):
+                    if group['variant'] == 'inc':
                         v_k.mul_((group['step'] / (group['step'] + 1)) ** 2)
                     v_k.add_(delta_k_sq)
                 else:
@@ -116,7 +98,7 @@ class A2Grad(Optimizer, BaseOptimizer):
                     torch.max(v_kk, v_k, out=v_k)
 
                 h_k = v_k.sqrt()
-                if self.variant != 'uni':
+                if group['variant'] != 'uni':
                     h_k.mul_(math.sqrt(group['step'] + 1))
 
                 coefficient = -1.0 / (gamma_k + group['beta'] * h_k.item())
