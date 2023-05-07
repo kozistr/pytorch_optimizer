fe62f52a885d6586d4e038e049e77d90ca3463e6
kozistr <kozistr@gmail.com>
2023-05-07 13:37:30 +0900

    update: test_load_optimizers_invalid

--- tests/test_load_lr_schedulers.py
+++ tests/test_load_lr_schedulers.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pytorch_optimizer import get_supported_lr_schedulers, load_lr_scheduler
-from tests.constants import INVALID_LR_SCHEDULER_NAMES, VALID_LR_SCHEDULER_NAMES
+from tests.constants import VALID_LR_SCHEDULER_NAMES
 
 
 @pytest.mark.parametrize('valid_lr_scheduler_names', VALID_LR_SCHEDULER_NAMES)
@@ -9,10 +9,9 @@ def test_load_optimizers_valid(valid_lr_scheduler_names):
     load_lr_scheduler(valid_lr_scheduler_names)
 
 
-@pytest.mark.parametrize('invalid_lr_scheduler_names', INVALID_LR_SCHEDULER_NAMES)
-def test_load_optimizers_invalid(invalid_lr_scheduler_names):
+def test_load_optimizers_invalid():
     with pytest.raises(NotImplementedError):
-        load_lr_scheduler(invalid_lr_scheduler_names)
+        load_lr_scheduler('dummy')
 
 
 def test_get_supported_lr_schedulers():
