ba039ba3f8e7799f638946fffd09549e682a6ab1
kozistr <kozistr@gmail.com>
2023-05-07 17:53:32 +0900

    update: recipe

--- tests/constants.py
+++ tests/constants.py
@@ -149,7 +149,7 @@ OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (SGDP, {'lr': 5e-1, 'weight_decay': 1e-4, 'nesterov': True}, 10),
     (Ranger, {'lr': 1e0, 'weight_decay': 1e-3}, 75),
     (Ranger, {'lr': 5e0, 'weight_decay': 1e-3, 'degenerated_to_sgd': True}, 5),
-    (Ranger21, {'lr': 7.5e-1, 'weight_decay': 1e-3, 'num_iterations': 100}, 100),
+    (Ranger21, {'lr': 1e0, 'weight_decay': 1e-3, 'num_iterations': 100}, 100),
     (Shampoo, {'lr': 5e-1, 'weight_decay': 1e-3, 'momentum': 0.1}, 10),
     (
         ScalableShampoo,
