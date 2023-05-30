3b9821e4467ab505312fb4e3217cc8bb557aea3b
kozistr <kozistr@gmail.com>
2023-05-30 13:53:42 +0900

    docs: Sophia usage

--- pytorch_optimizer/optimizer/sophia.py
+++ pytorch_optimizer/optimizer/sophia.py
@@ -9,10 +9,38 @@ from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMET
 class Sophia(Optimizer, BaseOptimizer):
     r"""A Scalable Stochastic Second-order Optimizer for Language Model Pre-training.
 
+        Sophia optimizer needs to update the hessian matrix periodically in training code.
+
+        ::python-code
+
+            k: int = 10  # update period
+            num_iters: int = 0
+            for epoch in epochs:
+                for x, y in dataloader:
+                    logits = model(x)
+
+                    loss = loss_fn(logits, y)
+                    loss.backward()
+
+                    optimizer.step(bs=4096)  # important! need to pass batch size.
+                    optimizer.zero_grad()
+
+                    if num_iters % k == 1:
+                        sampled_logits = torch.distributions.Categorical(logits=logits).sample()
+
+                        sampled_loss = loss_fn(logits, sampled_logits)
+                        sampled_loss.backward()
+
+                        optimizer.update_hessian()
+                        optimizer.zero_grad()
+
+                    num_iters += 1
+
     :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
     :param lr: float. learning rate.
     :param betas: BETAS. coefficients used for computing running averages of gradient and the squared hessian trace.
-    :param rho: float.
+    :param rho: float. rho. 0.03 for 125M Sophia-G, 0.05 for 0.05 for 355M Sophia-G.
+        Assume, larger model, bigger rho needs.
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
     :param fixed_decay: bool. fix weight decay.
