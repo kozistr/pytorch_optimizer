33038333ec948d3c95f9bc0de4a87183e406ff0c
kozistr <kozistr@gmail.com>
2023-06-07 20:22:54 +0900

    refactor: SophiaH optimizer

--- pytorch_optimizer/optimizer/sophiah.py
+++ pytorch_optimizer/optimizer/sophiah.py
@@ -1,15 +1,17 @@
+from typing import List, Optional
+
 import torch
 from torch.optim.optimizer import Optimizer
 
 from pytorch_optimizer.base.exception import NoSparseGradientError
 from pytorch_optimizer.base.optimizer import BaseOptimizer
-from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS, HUTCHINSON_G
+from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, HUTCHINSON_G, LOSS, PARAMETERS
 
 
 class SophiaH(Optimizer, BaseOptimizer):
     r"""Second-order Clipped Stochastic Optimization
 
-    Requires `loss.backward(create_graph=True)` in order to calculate hessians
+        Requires `loss.backward(create_graph=True)` in order to calculate hessians.
 
     :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
     :param lr: float. learning rate.
@@ -17,32 +19,39 @@ class SophiaH(Optimizer, BaseOptimizer):
     :param weight_decay: float. weight decay (L2 penalty).
     :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
     :param fixed_decay: bool. fix weight decay.
-    :param p: float. clip effective (applied) gradient (p)
-    :param update_period: int. number of steps after which to apply hessian approximation
-    :param n_samples: int. times to sample `z` for the approximation of the hessian trace
+    :param p: float. clip effective (applied) gradient (p).
+    :param update_period: int. number of steps after which to apply hessian approximation.
+    :param num_samples: int. times to sample `z` for the approximation of the hessian trace.
     :param eps: float. term added to the denominator to improve numerical stability.
     """
 
-    def __init__(self,
-                 params: PARAMETERS,
-                 lr: float = 6e-2,
-                 betas: BETAS = (0.96, 0.99),
-                 weight_decay: float = 0.0,
-                 weight_decouple: bool = True,
-                 fixed_decay: bool = False,
-                 p: float = 1e-2,
-                 update_period: int = 10,
-                 n_samples: int = 1,
-                 hessian_distribution: HUTCHINSON_G = 'gaussian',
-                 eps: float = 1e-12):
-
+    def __init__(
+        self,
+        params: PARAMETERS,
+        lr: float = 6e-2,
+        betas: BETAS = (0.96, 0.99),
+        weight_decay: float = 0.0,
+        weight_decouple: bool = True,
+        fixed_decay: bool = False,
+        p: float = 1e-2,
+        update_period: int = 10,
+        num_samples: int = 1,
+        hessian_distribution: HUTCHINSON_G = 'gaussian',
+        eps: float = 1e-12,
+    ):
         self.validate_learning_rate(lr)
         self.validate_betas(betas)
         self.validate_non_negative(weight_decay, 'weight_decay')
+        self.validate_non_negative(p, 'p (gradient clip)')
+        self.validate_step(update_period, 'update_period')
+        self.validate_positive(num_samples, 'num_samples')
+        self.validate_options(hessian_distribution, ('gaussian', 'rademacher'))
         self.validate_non_negative(eps, 'eps')
-        self.validate_non_negative(p, "p (gradient clip)")
 
+        self.update_period = update_period
+        self.num_samples = num_samples
         self.distribution = hessian_distribution
+
         defaults: DEFAULTS = {
             'lr': lr,
             'betas': betas,
@@ -52,44 +61,48 @@ class SophiaH(Optimizer, BaseOptimizer):
             'p': p,
             'eps': eps,
         }
-        self.n_samples = n_samples
-        self.update_period = update_period
-        self._step = 0
         super().__init__(params, defaults)
 
     @torch.no_grad()
     def reset(self):
-        self._step = 0
         for group in self.param_groups:
+            group['step'] = 0
             for p in group['params']:
                 state = self.state[p]
                 state['momentum'] = torch.zeros_like(p)
                 state['hessian_moment'] = torch.zeros_like(p)
 
     @torch.no_grad()
-    def step(self, closure: CLOSURE = None, hessian: tuple[torch.Tensor] = None) -> LOSS:
+    def step(self, closure: CLOSURE = None, hessian: Optional[List[torch.Tensor]] = None) -> LOSS:
         loss: LOSS = None
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
 
+        step: int = self.param_groups[0]['step']
+
         if hessian is not None:
             self.set_hessian(hessian)
-        elif self._step % self.update_period == 0:
-            self.compute_hutchinson_hessian(self.n_samples, distribution=self.distribution)
+        elif step % self.update_period == 0:
+            self.compute_hutchinson_hessian(self.num_samples, distribution=self.distribution)
 
         for group in self.param_groups:
+            if 'step' in group:
+                group['step'] += 1
+            else:
+                group['step'] = 1
+
+            beta1, beta2 = group['betas']
             for p in group['params']:
                 if p.grad is None:
                     continue
 
-                state = self.state[p]
                 grad = p.grad
                 if grad.is_sparse:
                     raise NoSparseGradientError(str(self))
 
-                # State initialization
-                if 'momentum' not in state:
+                state = self.state[p]
+                if len(state) == 0:
                     state['momentum'] = torch.zeros_like(p)
                     state['hessian_moment'] = torch.zeros_like(p)
 
@@ -102,18 +115,13 @@ class SophiaH(Optimizer, BaseOptimizer):
                     fixed_decay=group['fixed_decay'],
                 )
 
-                beta1, beta2 = group['betas']
                 momentum, hessian_moment = state['momentum'], state['hessian_moment']
+                momentum.mul_(beta1).add_(grad, alpha=1.0 - beta1)
 
-                momentum.mul_(beta1).add_(p.grad, alpha=1.0-beta1)
-                if (self._step % self.update_period == 0 or hessian is not None) and 'hessian' in state:
-                    hessian_moment.mul_(beta2).add_(state['hessian'], alpha=1.0-beta2)
+                if 'hessian' in state and (group['step'] % self.update_period == 0 or hessian is not None):
+                    hessian_moment.mul_(beta2).add_(state['hessian'], alpha=1.0 - beta2)
 
-                # See https://shreyansh26.github.io/post/2023-05-28_sophia_scalable_second_order_optimizer_llms/#per-coordinate-clipping
-                # The official implementation uses a different method to achieve the same thing (might be faster?):
-                # https://github.com/Liuhong99/Sophia/blob/bff9df9b584e2084fe037af1ab38f4db31f0acca/sophia.py#L201
-                update = torch.clip(momentum/torch.clip(hessian_moment, group['eps']), -group['p'], group['p'])
+                update = (momentum / torch.clip(hessian_moment, min=group['eps'])).clamp_(-group['p'], group['p'])
                 p.add_(update, alpha=-group['lr'])
 
-        self._step += 1
         return loss
