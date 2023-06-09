9ad0c80154f317b556eedf58b6dfb388f78e2e61
kozistr <kozistr@gmail.com>
2023-06-09 18:05:08 +0900

    update: recipe

--- tests/constants.py
+++ tests/constants.py
@@ -392,6 +392,7 @@ ADANORM_SUPPORTED_OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]]
     (Lion, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 5),
     (Yogi, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 5),
     (AdaMax, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 5),
+    (SWATS, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 5),
 ]
 ADAMD_SUPPORTED_OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (AdaBelief, {'lr': 1e1, 'weight_decay': 1e-3, 'adam_debias': True}, 5),
