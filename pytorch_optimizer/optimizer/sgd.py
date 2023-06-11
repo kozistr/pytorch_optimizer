b3eec107f37f1ebef5addfe516490a7d33da3ea9
kozistr <kozistr@gmail.com>
2023-06-11 16:27:02 +0900

    update: apply_weight_decay

--- pytorch_optimizer/optimizer/sgd.py
+++ pytorch_optimizer/optimizer/sgd.py
@@ -50,6 +50,7 @@ class AccSGD(Optimizer, BaseOptimizer):
     @torch.no_grad()
     def reset(self):
         for group in self.param_groups:
+            group['step'] = 0
             for p in group['params']:
                 state = self.state[p]
 
@@ -86,8 +87,14 @@ class AccSGD(Optimizer, BaseOptimizer):
                 if len(state) == 0:
                     state['momentum_buffer'] = p.clone()
 
-                if group['weight_decay'] > 0.0:
-                    grad.add_(p, alpha=group['weight_decay'])
+                self.apply_weight_decay(
+                    p,
+                    grad,
+                    lr=group['lr'],
+                    weight_decay=group['weight_decay'],
+                    weight_decouple=False,
+                    fixed_decay=False,
+                )
 
                 buf = state['momentum_buffer']
                 buf.mul_((1.0 / beta) - 1.0).add_(grad, alpha=-large_lr).add_(p).mul_(beta)
