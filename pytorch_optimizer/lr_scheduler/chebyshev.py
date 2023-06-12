1138f7a579b222c67b332cec6430a1e4f86bd6b4
kozistr <kozistr@gmail.com>
2023-06-12 20:36:29 +0900

    update: get_chebyshev_steps

--- pytorch_optimizer/lr_scheduler/chebyshev.py
+++ pytorch_optimizer/lr_scheduler/chebyshev.py
@@ -4,13 +4,15 @@ import numpy as np
 def get_chebyshev_steps(num_epochs: int, small_m: float = 0.05, big_m: float = 1.0) -> np.ndarray:
     r"""Chebyshev steps.
 
+        gamma_{t} = (M + m) / 2.0 - (M - m) * cos ((t - 0.5) * pi / T) / 2, where t = 1, ..., T
+
     :param num_epochs: int. stands for 'T' notation.
     :param small_m: float. stands for 'm' notation.
     :param big_m:  float. stands for 'M' notation.
     :return: np.array. chebyshev_steps.
     """
     c, r = (big_m + small_m) / 2.0, (big_m - small_m) / 2.0
-    thetas = (np.arange(num_epochs) + 0.5) / num_epochs * np.pi
+    thetas = (np.arange(num_epochs) + 0.5) * np.pi / num_epochs  # epoch starts from 0, so +0.5 instead of -0.5
 
     return 1.0 / (c - r * np.cos(thetas))
 
@@ -29,18 +31,18 @@ def get_chebyshev_permutation(num_epochs: int) -> np.ndarray:
     return perm
 
 
-def get_chebyshev_schedule(num_epochs: int) -> np.ndarray:
+def get_chebyshev_schedule(epoch: int, num_epochs: int) -> np.ndarray:
     r"""Get Chebyshev schedules.
 
-    :param num_epochs: int. number of epochs.
+    :param epoch: int. current epoch.
+    :param num_epochs: int. number of total epochs.
     """
-    if num_epochs < 1:
-        raise ValueError(f'[-] num_epochs must be over 1. {num_epochs} > 1')
-    elif num_epochs == 1:
-        return get_chebyshev_steps(1)
+    if epoch < 2:
+        return get_chebyshev_steps(1, num_epochs)
 
-    steps: np.ndarray = get_chebyshev_steps(num_epochs - 2)
+    steps: np.ndarray = get_chebyshev_steps(epoch, num_epochs - 2)
     perm: np.ndarray = get_chebyshev_permutation(num_epochs - 2)
+    print(len(steps), len(perm))
 
     return steps[perm]
 
