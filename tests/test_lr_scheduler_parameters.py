6d1811815c29473ad202f40c901deac071148dce
kozistr <kozistr@gmail.com>
2023-06-12 22:05:54 +0900

    update: test_chebyshev_params

--- tests/test_lr_scheduler_parameters.py
+++ tests/test_lr_scheduler_parameters.py
@@ -61,5 +61,5 @@ def test_linear_warmup_lr_scheduler_params():
 
 
 def test_chebyshev_params():
-    with pytest.raises(ValueError):
+    with pytest.raises(IndexError):
         get_chebyshev_schedule(0)
