cf5ee0d7790cc56ad6247330303e9c8b1906ed35
kozistr <kozistr@gmail.com>
2023-06-13 15:26:46 +0900

    refactor: p.grad to grad

--- pytorch_optimizer/optimizer/adasmooth.py
+++ pytorch_optimizer/optimizer/adasmooth.py
@@ -92,7 +92,7 @@ class AdaSmooth(Optimizer, BaseOptimizer):
 
                 self.apply_weight_decay(
                     p=p,
-                    grad=p.grad,
+                    grad=grad,
                     lr=group['lr'],
                     weight_decay=group['weight_decay'],
                     weight_decouple=group['weight_decouple'],
