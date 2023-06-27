ee4f3b2b28c0ae95db89d43b3e1f51253a0ba06f
kozistr <kozistr@gmail.com>
2023-06-27 15:26:27 +0900

    update: test_dice_loss

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -5,6 +5,7 @@ import torch
 from pytorch_optimizer import BCEFocalLoss, BCELoss, DiceLoss, FocalLoss, LDAMLoss, SoftF1Loss
 
 
+@torch.no_grad()
 @pytest.mark.parametrize('recipe', [('train', 0.37069410), ('eval', 0.30851572)])
 def test_bce_loss(recipe):
     mode, expected_loss = recipe
@@ -19,6 +20,7 @@ def test_bce_loss(recipe):
     assert loss == expected_loss
 
 
+@torch.no_grad()
 @pytest.mark.parametrize(
     'recipe',
     [
@@ -41,6 +43,7 @@ def test_bce_focal_loss(recipe):
     assert loss == expected_loss
 
 
+@torch.no_grad()
 def test_focal_loss():
     criterion = FocalLoss(alpha=1.0, gamma=2.0)
 
@@ -51,8 +54,58 @@ def test_focal_loss():
     np.testing.assert_almost_equal(loss.item(), 0.07848126)
 
 
+@torch.no_grad()
 def test_soft_f1_loss():
     criterion = SoftF1Loss()
+
+    y_pred = torch.sigmoid(torch.arange(-1.0, 1.0, 0.2))
+    y_true = torch.FloatTensor([0.0] * 5 + [1.0] * 5)
+    loss = criterion(y_pred, y_true)
+
+    np.testing.assert_almost_equal(loss.item(), 0.38905364)
+
+
+@torch.no_grad()
+def test_dice_loss():
+    eps: float = 1e-6
+    criterion = DiceLoss(mode='binary', from_logits=False, label_smooth=0.01)
+
+    # Ideal case
+    y_pred = torch.tensor([1.0, 1.0, 1.0]).view(1, 1, 1, -1)
+    y_true = torch.tensor(([1, 1, 1])).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.0, abs=eps)
+
+    y_pred = torch.tensor([1.0, 0.0, 1.0]).view(1, 1, 1, -1)
+    y_true = torch.tensor(([1, 0, 1])).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.0, abs=eps)
+
+    y_pred = torch.tensor([0.0, 0.0, 0.0]).view(1, 1, 1, -1)
+    y_true = torch.tensor(([0, 0, 0])).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.0, abs=eps)
+
+    # Worst case
+    y_pred = torch.tensor([1.0, 1.0, 1.0]).view(1, 1, -1)
+    y_true = torch.tensor([0, 0, 0]).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.0, abs=eps)
+
+    y_pred = torch.tensor([1.0, 0.0, 1.0]).view(1, 1, -1)
+    y_true = torch.tensor([0, 1, 0]).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.996677, abs=eps)
+
+    y_pred = torch.tensor([0.0, 0.0, 0.0]).view(1, 1, -1)
+    y_true = torch.tensor([1, 1, 1]).view(1, 1, 1, -1)
+    loss = criterion(y_pred, y_true)
+    assert float(loss) == pytest.approx(0.996677, abs=eps)
+
+
+@torch.no_grad()
+def test_ldam_loss():
+    criterion = LDAMLoss()
 
     y_pred = torch.sigmoid(torch.arange(-1.0, 1.0, 0.2))
     y_true = torch.FloatTensor([0.0] * 5 + [1.0] * 5)
