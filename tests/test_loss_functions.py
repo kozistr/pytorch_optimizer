b511de6982db4bd7dda1bd656aa731b6d5a011f5
kozistr <kozistr@gmail.com>
2023-06-27 15:19:05 +0900

    update: test_focal_loss

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -1,7 +1,8 @@
+import numpy as np
 import pytest
 import torch
 
-from pytorch_optimizer import BCEFocalLoss, BCELoss
+from pytorch_optimizer import BCEFocalLoss, BCELoss, DiceLoss, FocalLoss, LDAMLoss, SoftF1Loss
 
 
 @pytest.mark.parametrize('recipe', [('train', 0.37069410), ('eval', 0.30851572)])
@@ -38,3 +39,13 @@ def test_bce_focal_loss(recipe):
     loss = criterion(y_pred, y_true)
 
     assert loss == expected_loss
+
+
+def test_focal_loss():
+    criterion = FocalLoss(alpha=1.0, gamma=2.0)
+
+    y_pred = torch.arange(-1.0, 1.0, 0.2)
+    y_true = torch.FloatTensor([0.0] * 5 + [1.0] * 5)
+    loss = criterion(y_pred, y_true)
+
+    np.testing.assert_almost_equal(loss.item(), 0.07848126)
