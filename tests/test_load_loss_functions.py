282c5428b3b7f98c1c0f8c96eb676af9e846f309
kozistr <kozistr@gmail.com>
2023-06-27 15:12:39 +0900

    update: test_get_supported_loss_functions

--- tests/test_load_loss_functions.py
+++ tests/test_load_loss_functions.py
@@ -2,4 +2,4 @@ from pytorch_optimizer import get_supported_loss_functions
 
 
 def test_get_supported_loss_functions():
-    assert len(get_supported_loss_functions()) == 5
+    assert len(get_supported_loss_functions()) == 6
