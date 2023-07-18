e45912959aee6e7d0771cf1825a9eb912532873b
kozistr <kozistr@gmail.com>
2023-07-18 19:28:50 +0900

    update: recipe

--- tests/constants.py
+++ tests/constants.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, List, Tuple, Union
 
 from pytorch_optimizer import (
     ASGD,
+    CAME,
     LARS,
     MADGRAD,
     MSVAG,
@@ -389,6 +390,7 @@ OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (Prodigy, {'lr': 1e0, 'beta3': 0.999, 'weight_decay': 1e-3, 'safeguard_warmup': True}, 15),
     (PAdam, {'lr': 1e0, 'weight_decay': 1e-3}, 5),
     (Tiger, {'lr': 1e0, 'weight_decay': 1e-3}, 5),
+    (CAME, {'lr': 1e0, 'weight_decay': 1e-3}, 5),
 ]
 ADANORM_SUPPORTED_OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (AdaBelief, {'lr': 5e-1, 'weight_decay': 1e-3, 'adanorm': True}, 10),
