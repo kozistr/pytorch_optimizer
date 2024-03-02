c3a2fc6279476ed8fcf38d42ab41e5d31b069868
kozistr <kozistr@gmail.com>
2024-03-02 17:14:57 +0900

    feature: implement Aida optimizer

---
+++ pytorch_optimizer/optimizer/aida.py
@@ -0,0 +1,198 @@
+import math
+
+import torch
+from torch.optim.optimizer import Optimizer
+
+from pytorch_optimizer.base.exception import NoSparseGradientError
+from pytorch_optimizer.base.optimizer import BaseOptimizer
+from pytorch_optimizer.base.types import BETAS, CLOSURE, DEFAULTS, LOSS, PARAMETERS
+
+
+class Aida(Optimizer, BaseOptimizer):
+    r"""A DNN Optimizer that Improves over AdaBelief by Suppression of the Adaptive Stepsize Range.
+
+    :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
+    :param lr: float. learning rate.
+    :param betas: BETAS. coefficients used for computing running averages of gradient and the squared hessian trace.
+    :param k: int. number of vector projected per iteration.
+    :param xi: float. term used in vector projections to avoid division by zero.
+    :param weight_decay: float. weight decay (L2 penalty).
+    :param weight_decouple: bool. the optimizer uses decoupled weight decay as in AdamW.
+    :param fixed_decay: bool. fix weight decay.
+    :param rectify: bool. perform the rectified update similar to RAdam.
+    :param n_sma_threshold: number of SMA threshold (recommended is 5).
+    :param degenerated_to_sgd: bool. perform SGD update when variance of gradient is high.
+    :param ams_bound: bool. whether to use the AMSBound variant.
+    :param r: float. EMA factor. between 0.9 ~ 0.99 is preferred.
+    :param adanorm: bool. whether to use the AdaNorm variant.
+    :param adam_debias: bool. Only correct the denominator to avoid inflating step sizes early in training.
+    :param eps: float. term added to the denominator to improve numerical stability.
+    """
+
+    def __init__(
+        self,
+        params: PARAMETERS,
+        lr: float = 1e-3,
+        betas: BETAS = (0.9, 0.999),
+        k: int = 2,
+        xi: float = 1e-20,
+        weight_decay: float = 0.0,
+        weight_decouple: bool = False,
+        fixed_decay: bool = False,
+        rectify: bool = False,
+        n_sma_threshold: int = 5,
+        degenerated_to_sgd: bool = True,
+        ams_bound: bool = False,
+        r: float = 0.95,
+        adanorm: bool = False,
+        adam_debias: bool = False,
+        eps: float = 1e-8,
+    ):
+        self.validate_learning_rate(lr)
+        self.validate_betas(betas)
+        self.validate_non_negative(k, 'k')
+        self.validate_non_negative(weight_decay, 'weight_decay')
+        self.validate_non_negative(xi, 'xi')
+        self.validate_non_negative(eps, 'eps')
+
+        self.k = k
+        self.xi = xi
+        self.n_sma_threshold = n_sma_threshold
+        self.degenerated_to_sgd = degenerated_to_sgd
+
+        defaults: DEFAULTS = {
+            'lr': lr,
+            'betas': betas,
+            'weight_decay': weight_decay,
+            'weight_decouple': weight_decouple,
+            'fixed_decay': fixed_decay,
+            'rectify': rectify,
+            'ams_bound': ams_bound,
+            'adanorm': adanorm,
+            'adam_debias': adam_debias,
+            'eps': eps,
+        }
+        if adanorm:
+            defaults.update({'r': r})
+
+        super().__init__(params, defaults)
+
+    def __str__(self) -> str:
+        return 'Aida'
+
+    @torch.no_grad()
+    def reset(self):
+        for group in self.param_groups:
+            group['step'] = 0
+            for p in group['params']:
+                state = self.state[p]
+
+                state['exp_avg'] = torch.zeros_like(p)
+                state['exp_avg_var'] = torch.zeros_like(p)
+                if group['adanorm']:
+                    state['exp_grad_norm'] = torch.zeros((1,), dtype=p.dtype, device=p.device)
+                if group['ams_bound']:
+                    state['max_exp_avg_var'] = torch.zeros_like(p)
+
+    @torch.no_grad()
+    def step(self, closure: CLOSURE = None) -> LOSS:
+        loss: LOSS = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            if 'step' in group:
+                group['step'] += 1
+            else:
+                group['step'] = 1
+
+            beta1, beta2 = group['betas']
+
+            bias_correction1: float = 1.0 - beta1 ** group['step']
+            bias_correction2_sq: float = math.sqrt(1.0 - beta2 ** group['step'])
+
+            step_size, n_sma = self.get_rectify_step_size(
+                is_rectify=group['rectify'],
+                step=group['step'],
+                lr=group['lr'],
+                beta2=beta2,
+                n_sma_threshold=self.n_sma_threshold,
+                degenerated_to_sgd=self.degenerated_to_sgd,
+            )
+
+            step_size = self.apply_adam_debias(
+                adam_debias=group['adam_debias'],
+                step_size=step_size,
+                bias_correction1=bias_correction1,
+            )
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                grad = p.grad
+                if grad.is_sparse:
+                    raise NoSparseGradientError(str(self))
+
+                state = self.state[p]
+                if len(state) == 0:
+                    state['exp_avg'] = torch.zeros_like(p)
+                    state['exp_avg_var'] = torch.zeros_like(p)
+                    if group['adanorm']:
+                        state['exp_grad_norm'] = torch.zeros((1,), dtype=grad.dtype, device=grad.device)
+                    if group['ams_bound']:
+                        state['max_exp_avg_var'] = torch.zeros_like(p)
+
+                self.apply_weight_decay(
+                    p=p,
+                    grad=grad,
+                    lr=group['lr'],
+                    weight_decay=group['weight_decay'],
+                    weight_decouple=group['weight_decouple'],
+                    fixed_decay=group['fixed_decay'],
+                )
+
+                s_grad = self.get_adanorm_gradient(
+                    grad=grad,
+                    adanorm=group['adanorm'],
+                    exp_grad_norm=state.get('exp_grad_norm', None),
+                    r=group.get('r', None),
+                )
+
+                exp_avg, exp_avg_var = state['exp_avg'], state['exp_avg_var']
+                exp_avg.mul_(beta1).add_(s_grad, alpha=1.0 - beta1)
+
+                proj_g = grad.detach().clone()
+                proj_m = exp_avg.detach().clone()
+
+                for _ in range(self.k):
+                    proj_sum_gm = torch.sum(torch.mul(proj_g, proj_m))
+
+                    scalar_g = proj_sum_gm / (torch.sum(torch.pow(proj_g, 2)).add_(self.xi))
+                    scalar_m = proj_sum_gm / (torch.sum(torch.pow(proj_m, 2)).add_(self.xi))
+
+                    proj_g.mul_(scalar_g)
+                    proj_m.mul_(scalar_m)
+
+                grad_residual = proj_m - proj_g
+                exp_avg_var.mul_(beta2).addcmul_(grad_residual, grad_residual, value=1.0 - beta2).add_(group['eps'])
+
+                de_nom = self.apply_ams_bound(
+                    ams_bound=group['ams_bound'],
+                    exp_avg_sq=exp_avg_var,
+                    max_exp_avg_sq=state.get('max_exp_avg_var', None),
+                    eps=group['eps'],
+                )
+
+                if not group['rectify']:
+                    de_nom.div_(bias_correction2_sq)
+                    p.addcdiv_(exp_avg, de_nom, value=-step_size)
+                    continue
+
+                if n_sma >= self.n_sma_threshold:
+                    p.addcdiv_(exp_avg, de_nom, value=-step_size)
+                elif step_size > 0:
+                    p.add_(exp_avg, alpha=-step_size)
+
+        return loss
