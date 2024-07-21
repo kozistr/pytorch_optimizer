dbef58311c1b7cb457c235b1a9506152976fb935
kozistr <kozistr@gmail.com>
2024-07-21 19:23:10 +0900

    refactor: gc

--- pytorch_optimizer/optimizer/gc.py
+++ pytorch_optimizer/optimizer/gc.py
@@ -1,12 +1,12 @@
 import torch
 
 
-def centralize_gradient(x: torch.Tensor, gc_conv_only: bool = False):
+def centralize_gradient(grad: torch.Tensor, gc_conv_only: bool = False) -> None:
     r"""Gradient Centralization (GC).
 
-    :param x: torch.Tensor. gradient.
+    :param grad: torch.Tensor. gradient.
     :param gc_conv_only: bool. 'False' for both conv & fc layers.
     """
-    size: int = x.dim()
+    size: int = grad.dim()
     if (gc_conv_only and size > 3) or (not gc_conv_only and size > 1):
-        x.add_(-x.mean(dim=tuple(range(1, size)), keepdim=True))
+        grad.add_(-grad.mean(dim=tuple(range(1, size)), keepdim=True))
