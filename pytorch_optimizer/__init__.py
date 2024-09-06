af4bfcda6292558c1504c9668008468314241c81
kozistr <kozistr@gmail.com>
2024-09-06 17:19:43 +0900

    update: SOAP optimizer

--- pytorch_optimizer/__init__.py
+++ pytorch_optimizer/__init__.py
@@ -114,6 +114,7 @@ from pytorch_optimizer.optimizer.shampoo_utils import (
     power_iteration,
 )
 from pytorch_optimizer.optimizer.sm3 import SM3
+from pytorch_optimizer.optimizer.soap import SOAP
 from pytorch_optimizer.optimizer.sophia import SophiaH
 from pytorch_optimizer.optimizer.srmm import SRMM
 from pytorch_optimizer.optimizer.swats import SWATS
@@ -210,6 +211,7 @@ OPTIMIZER_LIST: List[OPTIMIZER] = [
     AdaLOMO,
     AdamG,
     AdEMAMix,
+    SOAP,
 ]
 OPTIMIZERS: Dict[str, OPTIMIZER] = {str(optimizer.__name__).lower(): optimizer for optimizer in OPTIMIZER_LIST}
 
