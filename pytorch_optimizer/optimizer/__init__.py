3f4a3faf169f2f794e5ffd8d7faaa8d2520e228d
kozistr <kozistr@gmail.com>
2025-01-14 19:53:37 +0900

    update: support OrthoGrad optimizer

--- pytorch_optimizer/optimizer/__init__.py
+++ pytorch_optimizer/optimizer/__init__.py
@@ -298,6 +298,7 @@ def create_optimizer(
     weight_decay: float = 0.0,
     wd_ban_list: List[str] = ('bias', 'LayerNorm.bias', 'LayerNorm.weight'),
     use_lookahead: bool = False,
+    use_orthograd: bool = False,
     **kwargs,
 ):
     r"""Build optimizer.
@@ -307,7 +308,8 @@ def create_optimizer(
     :param lr: float. learning rate.
     :param weight_decay: float. weight decay.
     :param wd_ban_list: List[str]. weight decay ban list by layer.
-    :param use_lookahead: bool. use lookahead.
+    :param use_lookahead: bool. use Lookahead.
+    :param use_orthograd: bool. use OrthoGrad.
     """
     optimizer_name = optimizer_name.lower()
 
@@ -315,22 +317,25 @@ def create_optimizer(
         get_optimizer_parameters(model, weight_decay, wd_ban_list) if weight_decay > 0.0 else model.parameters()
     )
 
-    optimizer = load_optimizer(optimizer_name)
+    optimizer_class = load_optimizer(optimizer_name)
 
     if optimizer_name == 'alig':
-        optimizer = optimizer(parameters, max_lr=lr, **kwargs)
+        optimizer = optimizer_class(parameters, max_lr=lr, **kwargs)
     elif optimizer_name in {'lomo', 'adalomo', 'adammini'}:
-        optimizer = optimizer(model, lr=lr, **kwargs)
+        optimizer = optimizer_class(model, lr=lr, **kwargs)
     else:
-        optimizer = optimizer(parameters, lr=lr, **kwargs)
+        optimizer = optimizer_class(parameters, lr=lr, **kwargs)
 
     if use_lookahead:
-        optimizer = Lookahead(
+        return Lookahead(
             optimizer,
             k=kwargs.get('k', 5),
             alpha=kwargs.get('alpha', 0.5),
             pullback_momentum=kwargs.get('pullback_momentum', 'none'),
         )
+
+    if use_orthograd:
+        return OrthoGrad(optimizer, **kwargs)
 
     return optimizer
 
