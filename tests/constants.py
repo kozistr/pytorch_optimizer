76b785670638fc1099efd3805a8674a493c32dfd
kozistr <kozistr@gmail.com>
2025-02-02 15:46:21 +0900

    update: recipe

--- tests/constants.py
+++ tests/constants.py
@@ -165,6 +165,7 @@ BETA_OPTIMIZER_NAMES: List[str] = [
     'mars',
     'adatam',
     'focus',
+    'exadam',
 ]
 
 VALID_LR_SCHEDULER_NAMES: List[str] = [
@@ -189,7 +190,7 @@ INVALID_LR_SCHEDULER_NAMES: List[str] = ['dummy']
 
 OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (build_lookahead, {'lr': 5e-1, 'weight_decay': 1e-3}, 5),
-    (build_orthograd, {'lr': 5e-1, 'weight_decay': 1e-3}, 5),
+    (build_orthograd, {'lr': 7.5e-1, 'weight_decay': 1e-3}, 10),
     (AdaBelief, {'lr': 5e-1, 'weight_decay': 1e-3}, 5),
     (AdaBelief, {'lr': 5e-1, 'weight_decay': 1e-3, 'ams_bound': True}, 5),
     (AdaBelief, {'lr': 5e-1, 'weight_decay': 1e-3, 'weight_decouple': False}, 5),
