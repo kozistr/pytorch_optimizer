8ff7eab257248487dba15440d1532032fac7a4c2
kozistr <kozistr@gmail.com>
2025-02-19 23:44:25 +0900

    update: test cases

--- tests/constants.py
+++ tests/constants.py
@@ -564,7 +564,7 @@ OPTIMIZERS: List[Tuple[Any, Dict[str, Union[float, bool, int]], int]] = [
     (FOCUS, {'lr': 1e-1, 'weight_decay': 1e-3}, 5),
     (Kron, {'lr': 1e0, 'weight_decay': 1e-3}, 3),
     (EXAdam, {'lr': 1e-1, 'weight_decay': 1e-3}, 5),
-    (SCION, {'lr': 5e-1, 'constraint': False, 'weight_decay': 1e-3}, 5),
+    (SCION, {'lr': 5e-1, 'constraint': False, 'weight_decay': 1e-3}, 10),
     (SCION, {'lr': 1e-1, 'constraint': True}, 10),
     (Ranger25, {'lr': 1e-1}, 3),
     (Ranger25, {'lr': 1e-1, 't_alpha_beta3': 5}, 3),
