092a4d4714f78dc63c16f320690470b5bdb418fe
kozistr <kozistr@gmail.com>
2025-03-24 13:19:26 +0900

    refactor: SCION optimizer

--- pytorch_optimizer/optimizer/scion.py
+++ pytorch_optimizer/optimizer/scion.py
@@ -1,5 +1,6 @@
 import math
-from typing import Literal
+from enum import IntEnum
+from typing import Dict, Optional
 
 import torch
 
@@ -8,17 +9,308 @@ from pytorch_optimizer.base.optimizer import BaseOptimizer
 from pytorch_optimizer.base.type import CLOSURE, DEFAULTS, LOSS, PARAMETERS
 from pytorch_optimizer.optimizer.shampoo_utils import zero_power_via_newton_schulz_5
 
-LMO_TYPE = Literal['spectral', 'sign', 'col_norm', 'row_norm']
+
+class LMONorm(IntEnum):
+    r"""normalization types."""
+
+    NONE = 0
+    AUTO = 1
+    SPECTRAL = 2
+    SPECTRALCONV = 3
+    SIGN = 4
+    BIAS = 5
+    COL = 6
+    ROW = 7
+
+
+class Norm:
+    r"""Base class to perform norm onto Scion. This class does no norm."""
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        r"""Initialize parameter."""
+        return x
+
+    def lmo(self, grad: torch.Tensor) -> torch.Tensor:
+        r"""Get LMO."""
+        return grad
+
+
+class Col(Norm):
+    r"""col-wise normalization.
+
+    :param normalized: bool. normalize by the input dimension. use for non-input layers.
+    :param transpose: bool. transpose input before normalization. use for embedding layers which have a shape of
+        (vocab_size, embedding_dim)
+    """
+
+    def __init__(self, normalized: bool = False, transpose: bool = False) -> None:
+        self.normalized = normalized
+        self.transpose = transpose
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        dtype = x.dtype
+        if self.transpose:
+            x = x.transpose(0, 1)
+
+        torch.nn.init.normal_(x)
+
+        x.div_(x.norm(dim=0, keepdim=True)).mul_(math.sqrt(x.size(0)))
+        if self.normalized:
+            x.div_(x.size(1))
+
+        x = x.to(dtype=dtype)
+        if self.transpose:
+            x = x.transpose(0, 1)
+
+        return x
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        if self.transpose:
+            grad = grad.transpose(0, 1)
+
+        d_in, d_out = grad.size()
+
+        rms_value = torch.sqrt(torch.sum(grad.pow(2), dim=0, keepdim=True)) / math.sqrt(d_in)
+        if self.normalized:
+            rms_value.mul_(d_out)
+
+        grad /= rms_value.add_(eps)
+
+        if self.transpose:
+            grad = grad.transpose(0, 1)
+
+        return grad
+
+
+class Row(Norm):
+    r"""row-wise normalization.
+
+    :param normalized: bool. normalize by the input dimension. use for non-input layers.
+    :param transpose: bool. transpose input before normalization. use for embedding layers which have a shape of
+        (vocab_size, embedding_dim)
+    """
+
+    def __init__(self, normalized: bool = True, transpose: bool = False) -> None:
+        self.normalized = normalized
+        self.transpose = transpose
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        dtype = x.dtype
+        if self.transpose:
+            x = x.transpose(0, 1)
+
+        torch.nn.init.normal_(x)
+
+        x.div_(x.norm(dim=-1, keepdim=True))
+        if self.normalized:
+            x.div_(math.sqrt(x.size(-1)))
+
+        x = x.to(dtype=dtype)
+        if self.transpose:
+            x = x.transpose(0, 1)
+
+        return x
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        if self.transpose:
+            grad = grad.transpose(0, 1)
+
+        rms_value = torch.sqrt(torch.sum(grad.pow(2), dim=-1, keepdim=True))
+        if self.normalized:
+            rms_value.mul_(math.sqrt(grad.size(-1)))
+
+        grad /= rms_value.add_(eps)
+
+        if self.transpose:
+            grad = grad.transpose(0, 1)
+
+        return grad
+
+
+class BiasRMS(Norm):
+    r"""bias RMS."""
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.nn.init.zeros_(x)
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        rms_value = torch.sqrt(torch.sum(grad.pow(2), dim=0, keepdim=True))
+        grad /= rms_value.add_(eps)
+        return grad
+
+
+class SpectralConv(Norm):
+    r"""spectral-convolution normalization.
+
+    :param num_steps: int. number of steps of zero-power Newton-Schulz 5.
+    """
+
+    def __init__(self, num_steps: int = 5) -> None:
+        self.num_steps = num_steps
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        x_fp64 = x.double()
+
+        d_out, d_in, kernel_size, *_ = x_fp64.size()
+
+        for i in range(kernel_size):
+            for j in range(kernel_size):
+                torch.nn.init.orthogonal_(x_fp64[..., i, j])
+
+        x_fp64.mul_(math.sqrt(d_out / d_in) / (kernel_size**2))
+
+        return x_fp64.to(dtype=x.dtype)
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        grad = zero_power_via_newton_schulz_5(grad.view(len(grad), -1), self.num_steps).view(grad.shape)
+
+        d_out, d_in, kernel_size, *_ = grad.size()
+
+        grad *= math.sqrt(d_out / d_in) / (kernel_size**2)
+
+        return grad
+
+
+class Spectral(Norm):
+    r"""spectral normalization.
+
+    :param max_scale: bool. set upper bound (1.0) of the scale.
+    :param normalize: bool. normalize by the input dimension. use for non-input layers.
+    :param num_steps: int. number of steps of zero-power Newton-Schulz 5.
+    """
+
+    def __init__(self, max_scale: bool = False, normalize: bool = True, num_steps: int = 5) -> None:
+        self.max_scale = max_scale
+        self.normalize = normalize
+        self.num_steps = num_steps
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        x_fp64 = x.double()
+
+        torch.nn.init.orthogonal_(x_fp64)
+
+        d_out, d_in = x_fp64.size()
+
+        scale: float = math.sqrt(d_out / d_in) if self.normalize else math.sqrt(d_out)
+        if self.max_scale:
+            scale = max(1.0, scale)
+
+        x_fp64.mul_(scale)
+
+        return x_fp64.to(dtype=x.dtype)
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        grad = zero_power_via_newton_schulz_5(grad.view(len(grad), -1), self.num_steps).view(grad.shape)
+
+        d_out, d_in = grad.size()
+
+        scale: float = math.sqrt(d_out / d_in) if self.normalize else math.sqrt(d_out)
+        if self.max_scale:
+            scale = max(1.0, scale)
+
+        grad *= scale
+
+        return grad
+
+
+class Sign(Norm):
+    r"""sign normalization.
+
+    :param zero_init: bool. initialize with zero.
+    :param normalize: bool. normalize by the input dimension. use for non-input layers.
+    """
+
+    def __init__(self, zero_init: bool = False, normalize: bool = True) -> None:
+        self.zero_init = zero_init
+        self.normalize = normalize
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        if self.zero_init:
+            return torch.nn.init.zeros_(x)
+
+        d_in: int = x.size(1)
+
+        x = 2 * torch.randint(0, 2, x.shape, dtype=x.dtype, device=x.device) - 1
+        if self.normalize:
+            x.div_(d_in)
+
+        return x
+
+    def lmo(self, grad: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
+        d_in: int = grad.size(1)
+        return torch.sign(grad).div_(d_in) if self.normalize else torch.sign(grad)
+
+
+class Auto(Norm):
+    r"""choose Norm type automatically."""
+
+    def init(self, x: torch.Tensor) -> torch.Tensor:
+        ndim: int = x.ndim
+        if ndim in (0, 1):
+            return BiasRMS().init(x)
+        if ndim == 2:
+            return Spectral().init(x)
+        if ndim in (3, 4):
+            return SpectralConv().init(x)
+        raise NotImplementedError
+
+    def lmo(self, grad: torch.Tensor) -> torch.Tensor:
+        ndim: int = grad.ndim
+        if ndim in (0, 1):
+            return BiasRMS().lmo(grad)
+        if ndim == 2:
+            return Spectral().lmo(grad)
+        if ndim in (3, 4):
+            return SpectralConv().lmo(grad)
+        raise NotImplementedError
+
+
+def build_lmo_norm(norm_type: int, **kwargs) -> Norm:  # noqa: PLR0911
+    r"""Build LMONorm by given norm_type."""
+    if norm_type == LMONorm.AUTO:
+        return Auto()
+    if norm_type == LMONorm.SPECTRAL:
+        return Spectral(**kwargs)
+    if norm_type == LMONorm.SPECTRALCONV:
+        return SpectralConv(**kwargs)
+    if norm_type == LMONorm.SIGN:
+        return Sign(**kwargs)
+    if norm_type == LMONorm.BIAS:
+        return BiasRMS()
+    if norm_type == LMONorm.COL:
+        return Col(**kwargs)
+    if norm_type == LMONorm.ROW:
+        return Row(**kwargs)
+    return Norm()
 
 
 class SCION(BaseOptimizer):
     r"""Training Deep Learning Models with Norm-Constrained LMOs.
 
+    Example:
+        >>> radius = 50.0
+        >>> parameter_groups = [{
+        ...     'params': model.transformer.h.parameters(),
+        ...     'norm_type': 'spectral',
+        ...     'norm_kwargs': {},
+        ...     'scale': radius,
+        ... }, {
+        ...     'params': model.lm_head.parameters(),
+        ...     'norm_type': 'sign',
+        ...     'norm_kwargs': {},
+        ...     'scale': radius * 60.0,
+        ... }]
+        >>> optimizer = SCION(parameter_groups)
+
+        For more details, checkout here https://github.com/LIONS-EPFL/scion/tree/main?tab=readme-ov-file#examples
+
     :param params: PARAMETERS. iterable of parameters to optimize or dicts defining parameter groups.
     :param lr: float. learning rate.
-    :param momentum: float. momentum factor.
+    :param momentum: float. momentum factor. 1.0 - usual momentum.
     :param constraint: bool. whether to use a constraint SCG or not.
-    :param lmo_type: LMO_TYPE. supported LMO types.
+    :param norm_type: int. supported LMO norm types. 0 stands for no normalization and 1 stands for AUTO. 0 to 7.
+        please check LMONorm Enum class for the details.
+    :param norm_kwargs: Optional[Dict]. arguments for the Norm.
     :param scale: float. based on the usage of the original intend, 50.0 is used for Transformer block, and 3000.0 is
         used for others (e.g. Embedding, LM head)
     :param weight_decay: float. weight decay (L2 penalty).
@@ -31,7 +323,8 @@ class SCION(BaseOptimizer):
         lr: float = 1e-3,
         momentum: float = 0.1,
         constraint: bool = False,
-        lmo_type: LMO_TYPE = 'spectral',
+        norm_type: int = LMONorm.AUTO,
+        norm_kwargs: Optional[Dict] = None,
         scale: float = 1.0,
         weight_decay: float = 0.0,
         weight_decouple: bool = True,
@@ -40,13 +333,16 @@ class SCION(BaseOptimizer):
         self.validate_learning_rate(lr)
         self.validate_range(momentum, 'momentum', 0.0, 1.0, '(]')
         self.validate_positive(scale, 'scale')
-        self.validate_options(lmo_type, 'lmo_type', ['spectral', 'sign', 'col_norm', 'row_norm'])
+
+        if norm_kwargs is None:
+            norm_kwargs = {}
 
         defaults: DEFAULTS = {
             'lr': lr,
             'momentum': momentum,
             'constraint': constraint,
-            'lmo_type': lmo_type,
+            'norm_type': norm_type,
+            'norm_kwargs': norm_kwargs,
             'scale': scale,
             'weight_decay': weight_decay,
             'weight_decouple': weight_decouple,
@@ -58,32 +354,7 @@ class SCION(BaseOptimizer):
 
     @torch.no_grad()
     def reset(self):
-        for group in self.param_groups:
-            for p in group['params']:
-                state = self.state[p]
-                state['d'] = torch.zeros_like(p)
-
-    @staticmethod
-    def get_lmo_direction(grad: torch.Tensor, lmo_type: LMO_TYPE) -> torch.Tensor:
-        r"""Get LMO direction.
-
-        fallback to `sign`
-        """
-        d_out, d_in, *_ = grad.shape if grad.ndim > 1 else (grad.size(0), grad.size(0))
-
-        if lmo_type == 'spectral':
-            return (
-                zero_power_via_newton_schulz_5(grad.reshape(len(grad), -1))
-                .view(grad.shape)
-                .mul_(max(1.0, math.sqrt(d_out / d_in)))
-            )
-        if lmo_type == 'sign':
-            return torch.sign(grad).div_(d_in)
-        if lmo_type == 'col_norm':
-            return grad / torch.norm(grad, dim=0, keepdim=True).add_(1e-6)
-        if lmo_type == 'row_norm' and grad.ndim == 2:
-            return grad / torch.norm(grad, dim=1, keepdim=True).add_(1e-6)
-        return torch.sign(grad).div_(d_in)
+        pass
 
     @torch.no_grad()
     def step(self, closure: CLOSURE = None) -> LOSS:
@@ -93,7 +364,8 @@ class SCION(BaseOptimizer):
                 loss = closure()
 
         for group in self.param_groups:
-            step_size: float = -group['lr']
+            norm = build_lmo_norm(group['norm_type'], **group['norm_kwargs'])
+
             for p in group['params']:
                 if p.grad is None:
                     continue
@@ -109,10 +381,12 @@ class SCION(BaseOptimizer):
                 d = state['d']
                 d.mul_(1.0 - group['momentum']).add_(grad, alpha=group['momentum'])
 
-                update = self.get_lmo_direction(d, group['lmo_type'])
-                update.mul_(group['scale'])
+                update = norm.lmo(grad).mul_(group['scale'])
 
-                if not group['constraint']:
+                if group['constraint']:
+                    p.mul_(1.0 - group['lr'])
+
+                if not group['constraint'] and group['weight_decay'] > 0.0:
                     self.apply_weight_decay(
                         p,
                         grad,
@@ -122,8 +396,6 @@ class SCION(BaseOptimizer):
                         fixed_decay=False,
                     )
 
-                    p.add_(update, alpha=step_size)
-                else:
-                    p.mul_(1.0 - step_size).add_(update, alpha=step_size)
+                p.add_(update, alpha=-group['lr'])
 
         return loss
