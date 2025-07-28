f2819f1ff5d4a4160fd0a4da22226fe1b4f6f7ab
kozistr <kozistr@gmail.com>
2025-07-28 22:28:50 +0900

    fix: test_version_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -254,9 +254,9 @@ def test_version_utils():
     python_version = sys.version_info
 
     if python_version.minor < 9:
-        assert parse_pytorch_version(torch.__version__) == [2, 7, 1]
-    else:
         assert parse_pytorch_version(torch.__version__) == [2, 4, 1]
+    else:
+        assert parse_pytorch_version(torch.__version__) == [2, 7, 1]
 
     assert compare_versions('2.7.0', '2.4.0') >= 0
 
